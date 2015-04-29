809101c199b46ae520fac15d30478cc15e173ae0
Jeff Gostick <jeff@gostick.ca>
2015-04-29 15:01:42 -0400

    basic rev calculator using boxes randomly distributed through the media

--- REV.py
+++ REV.py
@@ -6,7 +6,7 @@ Created on Wed Apr 29 08:46:37 2015
 """
 path = 'C:\\Users\\Jeff\\Dropbox\\Shared\\LatticeBoltzmann\\XRayImages\\'
 file = 'SEGMENTED.1500x1500_XY'
-ext = 'tif'
+ext  = 'tif'
 
 import scipy as sp
 import scipy.ndimage as spim
@@ -15,18 +15,35 @@ import tifffile as tff
 
 tiffimg = tff.TIFFfile(path+file+'.'+ext)
 im = tiffimg.asarray()
+im = sp.swapaxes(im, 0, 2)
 
 # Choose N random center points
-N = 50
-Cx = sp.random.randint(0,sp.shape(im)[0],N)
-Cy = sp.random.randint(0,sp.shape(im)[1],N)
-Cz = sp.random.randint(0,sp.shape(im)[2],N)
+N = 500
+Cx = sp.random.randint(sp.shape(im)[0]/4,3*sp.shape(im)[0]/4,N)
+Cy = sp.random.randint(sp.shape(im)[1]/4,3*sp.shape(im)[1]/4,N)
+Cz = sp.random.randint(sp.shape(im)[2]/4,3*sp.shape(im)[2]/4,N)
+C = sp.vstack((Cx,Cy,Cz)).T
 
+#Find maximum radius allowable for each point
+Rmax = sp.array(C>sp.array(sp.shape(im))/2)
+Rlim = sp.zeros(sp.shape(Rmax))
+Rlim[Rmax[:,0],0] = sp.shape(im)[0]
+Rlim[Rmax[:,1],1] = sp.shape(im)[1]
+Rlim[Rmax[:,2],2] = sp.shape(im)[2]
+R = sp.absolute(C-Rlim)
+R = R.astype(sp.int_)
+Rmin = sp.amin(R,axis=1)
 
-
+vol = []
+size = []
+porosity = []
+for i in range(0,N):
+    for r in sp.arange(Rmin[i],1,-10):
+        imtemp = im[C[i,0]-r:C[i,0]+r,C[i,1]-r:C[i,1]+r,C[i,2]-r:C[i,2]+r]
+        vol.append(sp.size(imtemp))
+        size.append(2*r)
+        porosity.append(sp.sum(1-imtemp)/sp.size(imtemp))
 
 plt.figure(0)
-plt.subplot(1,2,1)
-plt.imshow(im)
-plt.subplot(1,2,2)
+plt.style.use('ggplot')
 plt.plot(size,porosity,'b.')
