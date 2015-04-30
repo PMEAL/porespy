0caf72edbb4808c96cbf0d705fdcf9c14739d617
Jeff Gostick <jeff@gostick.ca>
2015-04-30 12:06:12 -0400

    Fiddling with REV

--- porespy/__rev__.py
+++ porespy/__rev__.py
@@ -5,10 +5,10 @@ class REV(object):
     r'''
     '''
     
-    def run(self,im,N=500):
+    def run(self,im,N=100):
         r'''
         '''
-        pad = [0.1,0.1,0.45]
+        pad = [0.25,0.25,0.45]
         Cx = sp.random.randint(pad[0]*sp.shape(im)[0],(1-pad[0])*sp.shape(im)[0],N)
         Cy = sp.random.randint(pad[1]*sp.shape(im)[1],(1-pad[1])*sp.shape(im)[1],N)
         Cz = sp.random.randint(pad[2]*sp.shape(im)[2],(1-pad[2])*sp.shape(im)[2],N)
@@ -29,11 +29,11 @@ class REV(object):
         porosity = []
         for i in range(0,N):
             R = Rmin[i]
+            print(i/N)
             for r in sp.arange(Rmin[i],1,-10):
-                imtemp = im[:,:,C[i,2]-r:C[i,2]+r]
+                imtemp = im[C[i,0]-150:C[i,0]+150,C[i,1]-150:C[i,1]+150:,C[i,2]-r:C[i,2]+r]
                 vol.append(sp.size(imtemp))
                 size.append(2*r)
                 porosity.append(sp.sum(imtemp==0)/(sp.size(imtemp)))
-        #        porosity.append(sp.sum(imtemp==2)/(sp.sum(imtemp==0)+sp.sum(imtemp==2)))
         
         return [porosity,size]
--- run_script.py
+++ run_script.py
@@ -1,4 +1,6 @@
+import scipy as sp
 import porespy as ps
+import matplotlib.pyplot as plt
 
 path = 'C:\\Users\\Jeff\Dropbox\\Shared\\LatticeBoltzmann\\XRayImages\\'
 file = 'SEGMENTED_600x600_WholeThickness'
@@ -6,4 +8,7 @@ ext  = 'tif'
 
 im = ps.imopen(filename=path+file+'.'+ext)
 rev = ps.REV()
-[porosity,size] = rev.run(im)
+[porosity,size] = rev.run(im,N=300)
+plt.style.use('ggplot')
+plt.plot(size,porosity,'b.')
+sp.savetxt('rev.csv',sp.vstack((size,porosity)).T)
