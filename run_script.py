1012f0c47b8260bb8a615d16283bc0ee06149953
Jeff Gostick <jeff@gostick.ca>
2015-05-06 16:48:06 -0400

    Making run script more general

--- run_script.py
+++ run_script.py
@@ -1,16 +1,19 @@
 import scipy as sp
+import scipy.ndimage as spim
 import porespy as ps
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
-path = 'C:\\Users\\Jeff\Dropbox\\Shared\\LatticeBoltzmann\\XRayImages\\'
-file = 'SEGMENTED.1500x1500_XY'
-ext  = 'tif'
+#path = 'C:\\Users\\Jeff\Dropbox\\Shared\\LatticeBoltzmann\\XRayImages\\'
+#file = 'SEGMENTED.1500x1500_XY'
+#ext  = 'tif'
+#im = 1 - ps.imopen(filename=path+file+'.'+ext)
 
-im = 1 - ps.imopen(filename=path+file+'.'+ext)
+im = sp.rand(30,30,30)<0.998
+im = spim.distance_transform_bf(im)>4
 
-[distance, count] = ps.tpc.run(img=im,npoints=5000)
-plt.plot(distance,count)
+#[distance, count] = ps.tpc.run(img=im,npoints=5000)
+#plt.plot(distance,count)
 
 #[porosity,size] = ps.rev.run(im,N=100)
 #plt.plot(size,porosity,'b.')
