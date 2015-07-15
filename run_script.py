94094bf12ee51135a9c0e7df2d40ac23c9552726
Jeff Gostick <jeff@gostick.ca>
2015-07-15 13:36:00 -0400

    Updated api for rev class and added example to init. Also update runscript

--- run_script.py
+++ run_script.py
@@ -13,25 +13,34 @@ import timeit
 # im = imageio.mimread(path+'\\'+file+'.'+ext)
 #==============================================================================
 
-# Generate an image of spheres
 start_time = timeit.default_timer()
-im = sp.rand(100, 100, 100) < 0.997
+
+ Generate an image of spheres
+im = sp.rand(80, 80, 80) < 0.997
 im = spim.distance_transform_bf(im) >= 4
 print(timeit.default_timer() - start_time)
 
-# Perform CLD calculation and get image for illustration
-a = ps.cld(image=im)
-cx = a.xdir(spacing=5, trim_edges=True)
-print(timeit.default_timer() - start_time)
-cim = a.get_chords(direction='x', spacing=5, trim_edges=True)
-print(timeit.default_timer() - start_time)
+#==============================================================================
+# # Perform CLD calculation and get image for illustration
+# a = ps.cld(image=im)
+# cx = a.xdir(spacing=5, trim_edges=True)
+# print(timeit.default_timer() - start_time)
+# cim = a.get_chords(direction='x', spacing=5, trim_edges=True)
+# print(timeit.default_timer() - start_time)
+#
+# # Plot some slices
+# plt.subplot(2, 2, 1)
+# plt.imshow(im[:, :, 7])
+# plt.subplot(2, 2, 3)
+# plt.imshow(im[:, :, 7]*1.0 - cim[:, :, 7]*0.5)
+# plt.subplot(2, 2, 2)
+# plt.plot(cx)
+# plt.subplot(2, 2, 4)
+# plt.plot(sp.log10(cx))
+#==============================================================================
 
-# Plot some slices
-plt.subplot(2, 2, 1)
-plt.imshow(im[:, :, 7])
-plt.subplot(2, 2, 3)
-plt.imshow(im[:, :, 7]*1.0 - cim[:, :, 7]*0.5)
-plt.subplot(2, 2, 2)
-plt.plot(cx)
-plt.subplot(2, 2, 4)
-plt.plot(sp.log10(cx))
+# Perform REV calculation
+b = ps.rev(image=im)
+vals = b.run(N=500)
+# Plot results
+plt.plot(vals.size, vals.porosity, 'bo')
