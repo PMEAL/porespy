1ee7fd6443f27f7fdeab515c5aff173c3850586f
Jeff Gostick <jeff@gostick.ca>
2015-07-15 10:44:34 -0400

    Removed home cooked image io and now use the imageio package which has 3d tiff image reading

--- porespy/__init__.py
+++ porespy/__init__.py
@@ -1,10 +1,7 @@
 r'''
 The pore space image should be 1's for void and 0's for solid
 '''
-from .__imio__ import ImageIO
 from .__cld__ import ChordLengthDistribution as cld
 from .__tpc__ import TwoPointCorrelation as tpc
 from .__rev__ import RepresentativeElementaryVolume as rev
 
-imopen = ImageIO.imopen
-del ImageIO
