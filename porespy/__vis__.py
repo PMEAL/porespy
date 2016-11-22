ec907599838ca95aad288d755c1566326e442929
Jeff Gostick <jeff@gostick.ca>
2016-11-22 17:28:58 -0500

    Adding some handy quick visualization tools in a class called Visualization, starting with SEM view

---
+++ porespy/__vis__.py
@@ -0,0 +1,24 @@
+import scipy as sp
+from collections import namedtuple
+import scipy.ndimage as spim
+import skimage as skim
+from skimage import morphology
+
+
+class Visualizations(object):
+    r"""
+
+    """
+
+    @staticmethod
+    def sem(im, direction='X'):
+        im = sp.array(~im, dtype=int)
+        if direction in ['Y', 'y']:
+            im = sp.transpose(im, axes=[1,0,2])
+        if direction in ['Z', 'z']:
+            im = sp.transpose(im, axes=[2,1,0])
+        t = im.shape[0]
+        depth = sp.reshape(sp.arange(0, t), [t, 1, 1])
+        im = im*depth
+        im = sp.amax(im, axis=0)
+        return im
