cdb86a81e912f1ce33d640fdddaf601554457d58
Jeff Gostick <jeff@gostick.ca>
2016-12-31 21:29:44 -0500

    Added a much improved 'segmented image to pore network' extraction function, and moved lots of funcs around.

--- porespy/network_extraction/__funcs__.py
+++ porespy/network_extraction/__funcs__.py
@@ -5,8 +5,35 @@ from skimage.morphology import reconstruction, watershed
 from skimage.segmentation import find_boundaries
 from skimage.morphology import disk, square, ball, cube
 
+
+def maxima_filt(im, strel):
+    def f(vals):
+        vals[24] *= 0.9999
+        return sp.amax(vals)
+    a = spim.filters.generic_filter(input=im, function=f, footprint=disk(3))
+    return a
+
+
 def all_peaks(dt, r=3):
     r"""
+    Returns all local maxima in the distance transform.
+
+    Parameters
+    ----------
+    dt : ND-array
+        The distance transform of the pore space.  This may be calculated and
+        filtered using any means desired.
+
+    r : scalar
+        The radius of the structuring element used in the maximum filter.  This
+        controls the localness of any maxima. The default is 3 voxels, which
+        means that a voxel is considered maximal if no other voxel within
+        a radius of 3 voxels has a higher number.
+
+    Returns
+    -------
+    An ND-array of booleans with True values at the location of any local
+    maxima.
     """
     dt = dt.squeeze()
     im = dt > 0
@@ -21,6 +48,7 @@ def all_peaks(dt, r=3):
     mx = spim.maximum_filter(dt + 2*(~im), footprint=ball(r))
     peaks = (dt == mx)*im
     return peaks
+
 
 def partition_pore_space(dt, peaks):
     r"""
@@ -98,3 +126,19 @@ def trim_extrema(im, h, mode='maxima'):
         result = reconstruction(seed=im - h, mask=im, method='dilation')
         result = reconstruction(seed=result + h, mask=result, method='erosion')
     return result
+
+
+def bounding_box_indices(roi):
+    r"""
+    Given the ND-coordinates of voxels defining the region of interest, the
+    indices of a bounding box are returned.
+    """
+    maxs = sp.amax(roi, axis=1)
+    mins = sp.amin(roi, axis=1)
+    x = []
+    x.append(slice(mins[0], maxs[0]))
+    x.append(slice(mins[1], maxs[1]))
+    if sp.shape(roi)[0] == 3:  # If image if 3D, add third coordinate
+        x.append(slice(mins[2], maxs[2]))
+    inds = tuple(x)
+    return inds
--- porespy/network_extraction/__getnet__.py
+++ porespy/network_extraction/__getnet__.py
@@ -3,6 +3,7 @@ import scipy.ndimage as spim
 import scipy.sparse as sprs
 from skimage.segmentation import find_boundaries
 
+
 def extract_pore_network(im):
     r"""
     Analyzes an image that has been partitioned into pore regions and extracts
@@ -16,42 +17,70 @@ def extract_pore_network(im):
     directly to an OpenPNM network object using the ``update`` command.
     """
     from skimage.morphology import disk, square, ball, cube
-    dt = spim.distance_transform_edt(im > 0)
-
     if im.ndim == 2:
-        ball = disk
         cube = square
-    edges = find_boundaries(im)
+        ball = disk
+
+    # Process input image for determination of sizes
+    dt = spim.distance_transform_edt(im > 0)
     mn = spim.minimum_filter(im + (im == 0)*2*sp.amax(im), footprint=ball(1))
     mx = spim.maximum_filter(im, footprint=ball(1))
-    hits = sp.where((mn != mx) * (mx > 0))
-    conns = sp.vstack((mn[hits], mx[hits])).T - 1
-    data = sp.ones(sp.shape(conns)[0])
-    Np = sp.amax(conns) + 1
-    adjmat = sprs.coo_matrix((data, (conns[:,0], conns[:,1])), shape=[Np, Np])
-    adjmat = adjmat.tocsr().tocoo()
-    adjmat.data = sp.ones_like(adjmat.data)
-    Nt = adjmat.data.size
-    conns = sp.vstack([adjmat.row, adjmat.col]).T
+    im_w_throats = sp.copy(im)
+    inds = (mn != im)
+    im_w_throats[inds] = mn[inds]
+    inds = (mx != im)
+    im_w_throats[inds] = mx[inds]
+    im_w_throats *= im > 0
 
-    coords = sp.zeros((Np, im.ndim), dtype=int)
-    volume = sp.zeros((Np, ), dtype=int)
-    diameter = sp.zeros((Np, ), dtype=int)
-    for i in sp.unique(im)[1:]:
-        vxls = sp.where(im == i)
+    # Start extracting size information for pores and throats
+    Ps = sp.unique(im)[1:]
+    Np = sp.size(Ps)
+    p_coords = sp.zeros((Np, im.ndim), dtype=int)
+    p_volume = sp.zeros((Np, ), dtype=int)
+    p_diameter = sp.zeros((Np, ), dtype=int)
+    p_vxls = sp.empty((Np, ), dtype=object)
+    t_conns = []
+    t_diameter = []
+    t_vxls = []
+    t_coords = []
+    for i in Ps:
         pore = i - 1
-        coords[pore, :] = sp.mean(vxls, axis=1)
-        volume[pore] = vxls[0].size
-        diameter[pore] = sp.amax(dt[vxls])
-    if im.ndim == 2:
-        coords = sp.vstack((coords.T, sp.zeros((Np,)))).T
+        p_vxls[pore] = sp.where(im == i)
+        # Get pore info
+        p_coords[pore, :] = sp.mean(p_vxls[pore], axis=1)
+        p_volume[pore] = sp.size(p_vxls[pore][0])
+        p_diameter[pore] = sp.amax(dt[p_vxls[pore]])
+        # Get throat info
+        Pn = sp.unique(im_w_throats[p_vxls[pore]]) - 1
+        for j in Pn:
+            if j > pore:
+                t_conns.append([pore, j])
+                temp = im_w_throats[p_vxls[pore]] == (j + 1)
+                inds = tuple((p_vxls[pore][0][temp], p_vxls[pore][1][temp]))
+                t_vxls.append(inds)
+                t_diameter.append(sp.amax(dt[inds]))
+                temp = sp.where(dt[inds] == sp.amax(dt[inds]))[0][0]
+                t_coords.append(tuple((inds[0][temp], inds[1][temp])))
+    # Clean up values
+    Nt = len(t_vxls)  # Get number of throats
+    if im.ndim == 2:  # If 2D, add 0's in 3rd dimension
+        p_coords = sp.vstack((p_coords.T, sp.zeros((Np, )))).T
+        t_coords = sp.vstack((sp.array(t_coords).T, sp.zeros((Nt, )))).T
 
+    # Start creating dictionary of pore network information
     net = {}
     net['pore.all'] = sp.ones((Np, ), dtype=bool)
     net['throat.all'] = sp.ones((Nt, ), dtype=bool)
-    net['pore.coords'] = coords
-    net['throat.conns'] = conns
-    net['pore.volume'] = volume
-    net['pore.diameter'] = diameter
+    net['pore.coords'] = p_coords
+    net['throat.conns'] = sp.array(t_conns)
+    net['pore.volume'] = p_volume
+    net['throat.volume'] = sp.zeros((Nt, ), dtype=float)
+    net['pore.diameter'] = p_diameter
+    net['throat.diameter'] = sp.array(t_diameter)
+    net['throat.coords'] = sp.array(t_coords)
+    P12 = net['throat.conns']
+    P1 = net['pore.coords'][P12][:, 0]
+    P2 = net['pore.coords'][P12][:, 1]
+    net['throat.length'] = sp.sqrt(sp.sum((P1 - P2)**2, axis=1))
 
     return net
