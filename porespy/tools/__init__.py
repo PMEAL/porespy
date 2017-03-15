a921c8cbe39da5e5b9dec5c50cc97b5eaa1969e3
Jeff Gostick <jeff@gostick.ca>
2017-03-15 00:16:35 -0400

    Changing init of the tools module for new generic flood function

--- porespy/tools/__init__.py
+++ porespy/tools/__init__.py
@@ -1,1 +1,2 @@
-from .__funcs__ import find_edges, randomize_colors, get_border, make_contiguous
+from .__funcs__ import find_edges, randomize_colors, get_border
+from .__funcs__ import make_contiguous, flood, concentration_transform
