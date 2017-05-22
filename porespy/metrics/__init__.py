30b7538bee6d49cefd00450037551d28fa861c5b
Jeff Gostick <jeff@gostick.ca>
2017-05-22 17:53:44 -0400

    started adding some documentation to modules

--- porespy/metrics/__init__.py
+++ porespy/metrics/__init__.py
@@ -1,3 +1,12 @@
+r"""
+===============================================================================
+Metrics
+===============================================================================
+A suite of tools for determine key metrics about an image, including:
+
+``porosity`` - Quickly determines the ratio of void voxels to total voxels
+of the image.
+"""
 from .__cld__ import chord_length_distribution
 from .__tpc__ import two_point_correlation
 from .__psf__ import pore_size_function
