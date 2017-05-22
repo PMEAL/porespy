27f83349ee627100b31c222f82ed5873fdd07be4
Jeff Gostick <jeff@gostick.ca>
2017-05-22 17:50:16 -0400

    added some functions to the import of the network extraction module

--- porespy/network_extraction/__init__.py
+++ porespy/network_extraction/__init__.py
@@ -2,6 +2,7 @@ r"""
 This module contains a variety of network extraction algorithms
 """
 
-from .__snow2__ import snow
+from .__snow3__ import *
 from .__funcs__ import trim_extrema, partition_pore_space, all_peaks
+from .__funcs__ import align_images_with_openpnm
 from .__getnet__ import extract_pore_network
