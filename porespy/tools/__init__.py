e7575244fe43e8e14c7648ce8488844cef377250
Jeff Gostick <jeff@gostick.ca>
2017-06-12 23:41:25 -0400

    updating init file to import new functions

--- porespy/tools/__init__.py
+++ porespy/tools/__init__.py
@@ -1,4 +1,5 @@
 from .__funcs__ import find_edges, randomize_colors, get_border
 from .__funcs__ import make_contiguous, flood, flood2, binary_opening_fast
-from .__funcs__ import extend_slice, reduce_peaks_to_points
-from .__funcs__ import remove_disconnected_voxels, fill_blind_pores
+from .__funcs__ import extend_slice, reduce_peaks_to_points, extract_subsection
+from .__funcs__ import remove_disconnected_voxels
+from .__funcs__ import remove_blind_pores, remove_floating_solid
