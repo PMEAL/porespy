767531d95d4e57bf85b6c3272df5a04a264cb6b6
Jeff Gostick <jeff@gostick.ca>
2017-07-01 21:46:52 -0400

    Added two functions for adding chords to image.  One does 1D chords and works on 2 and 3 images, the other does all three principle directions at once and only works on 3D image.

--- porespy/metrics/__funcs__.py
+++ porespy/metrics/__funcs__.py
@@ -1,13 +1,106 @@
 import scipy as sp
+from skimage.segmentation import clear_border
+from scipy.ndimage import label
 
 
 def porosity(im):
     r'''
     '''
-    sv = sp.sum(im == 0)
-    pv = sp.sum(im > 0)
-    e = pv/(pv+sv)
+    e = sp.sum(im > 0)/im.size
     return e
+
+
+def apply_chords(im, spacing=0, axis=0, trim_edges=True):
+    r"""
+    Adds chords to the void space in the specified direction.  The chords are
+    separated by 1 voxels plus the provided spacing.
+
+    Parameters
+    ----------
+    im : ND-array
+        A 2D image of the porous material with void marked as True.
+
+    spacing : int (default = 0)
+        Chords are automatically separed by 1 voxel and this argument increases
+        the separation.
+
+    axis : int (default = 0)
+        The axis along which the chords are drawn.
+
+    trim_edges : bool (default = True)
+        Whether or not to remove chords that touch the edges of the image.
+        These chords are artifically shortened, so skew the chord length
+        distribution
+
+    Returns
+    -------
+    An ND-array of the same size as ```im``` with True values indicating the
+    chords.
+
+    See Also
+    --------
+    apply_chords_3D
+
+    """
+    if spacing < 0:
+        raise Exception('Spacing cannot be less than 0')
+    dims1 = sp.arange(0, im.ndim)
+    dims2 = sp.copy(dims1)
+    dims2[axis] = 0
+    dims2[0] = axis
+    im = sp.moveaxis(a=im, source=dims1, destination=dims2)
+    im = sp.atleast_3d(im)
+    ch = sp.zeros_like(im, dtype=bool)
+    ch[:, ::4+2*spacing, ::4+2*spacing] = 1
+    chords = im*ch
+    chords = sp.squeeze(chords)
+    if trim_edges:
+        temp = clear_border(label(chords == 1)[0]) > 0
+        chords = temp*chords
+    chords = sp.moveaxis(a=chords, source=dims1, destination=dims2)
+    return chords
+
+
+def apply_chords_3D(im, spacing=0, trim_edges=True):
+    r"""
+    Adds chords to the void space in all three principle directions.  The
+    chords are seprated by 1 voxel plus the provided spacing.  Chords in the X,
+    Y and Z directions are labelled 1, 2 and 3 resepctively.
+
+    Parameters
+    ----------
+    im : ND-array
+        A 3D image of the porous material with void space marked as True.
+
+    spacing : int (default = 0)
+        Chords are automatically separed by 1 voxel on all sides, and this
+        argument increases the separation.
+
+    trim_edges : bool (default = True)
+        Whether or not to remove chords that touch the edges of the image.
+        These chords are artifically shortened, so skew the chord length
+        distribution
+
+    Returns
+    -------
+    An ND-array of the same size as ```im``` with values of 1 indicating
+    x-direction chords, 2 indicating y-direction chords, and 3 indicating
+    z-direction chords.
+
+    """
+    if im.ndim < 3:
+        raise Exception('Must be a 3D image to use this function')
+    if spacing < 0:
+        raise Exception('Spacing cannot be less than 0')
+    ch = sp.zeros_like(im, dtype=int)
+    ch[:, ::4+2*spacing, ::4+2*spacing] = 1  # X-direction
+    ch[::4+2*spacing, :, 2::4+2*spacing] = 2  # Y-direction
+    ch[2::4+2*spacing, 2::4+2*spacing, :] = 3  # Z-direction
+    chords = ch*im
+    if trim_edges:
+        temp = clear_border(label(chords > 0)[0]) > 0
+        chords = temp*chords
+    return chords
 
 
 def size_distribution(im, bins=None, return_im=False):
