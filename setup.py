cb067f3cd229b741ce2159cf79679318b2d28fdb
Jeff Gostick <jeff@gostick.ca>
2017-07-05 21:55:36 -0400

    Adding travis config and test requirement files

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     ],
     install_requires = [
         'numpy',
-        'scipy>=0.14.0',
+        'scipy',
         'matplotlib',
         'imageio'
     ],
--- test/test_package.py
+++ test/test_package.py
@@ -6,6 +6,9 @@ class TestPackage():
     def setup_class(self):
         pass
 
-    def test_randomize_colors(self):
+    def test_version_number_and_git_tag_agree(self):
         repo = git.Repo(search_parent_directories=True)
-        assert ps.__version__ == repo.git.describe("--tags").strip('vV')
+        tag = repo.git.describe("--tags")
+        tag = tag.strip('vV')  # Remove 'v' or 'V' from tag if present
+        tag = tag.split('-')[0]  # Remove hash from tag number if present
+        assert ps.__version__ == tag
