930485fb6fe7ce752abaff2909061390fa97ccac
Jeff Gostick <jeff@gostick.ca>
2017-08-22 23:18:16 -0400

    Adding an export module, with stub code for a to_vtk function

---
+++ porespy/export/__funcs__.py
@@ -0,0 +1,10 @@
+import scipy as sp
+
+
+def to_vtk(im, filename):
+    r"""
+    Converts an ND-array image to a file suitable for visualization in VTK
+    compatible applications such as ParaView.
+
+    """
+    pass
