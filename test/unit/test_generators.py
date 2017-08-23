2cc51d80c7adaa9871b97255c5b6b0cba8a103af
Jeff Gostick <jeff@gostick.ca>
2017-08-22 22:12:45 -0400

    fixing up some units tests...everything works locally

--- test/unit/test_generators.py
+++ test/unit/test_generators.py
@@ -8,13 +8,15 @@ class GeneratorTest():
     def setup_class(self):
         sp.random.seed(0)
 
-    def test_fibers(self):
+    def test_cylinders(self):
+        X = 100
+        Y = 100
         # Fibers don't work in 2D
-        with pytest.raises(ValueError):
-            im = ps.generators.fibers(shape=[100, 100], radius=4, nfibers=20)
+        with pytest.raises(Exception):
+            im = ps.generators.cylinders(shape=[X, Y], radius=4, nfibers=20)
         # But this works
-        im = ps.generators.fibers(shape=[1, 100, 100], radius=1, nfibers=20)
+        im = ps.generators.cylinders(shape=[1, X, Y], radius=1, nfibers=20)
         assert im.dtype == bool
-        assert sp.shape(im.squeeze()) == (100, 100)
-        im = ps.generators.fibers(shape=[50, 50, 50], radius=1, nfibers=20)
+        assert sp.shape(im.squeeze()) == (X, Y)
+        im = ps.generators.cylinders(shape=[50, 50, 50], radius=1, nfibers=20)
         assert sp.shape(im.squeeze()) == (50, 50, 50)
--- test/unit/test_visualization.py
+++ test/unit/test_visualization.py
@@ -2,23 +2,11 @@ import porespy as ps
 import scipy as sp
 
 
-class ToolsTest():
+class VisualizationTest():
     def setup_class(self):
         pass
 
-    def test_randomize_colors(self):
-        im = sp.random.randint(0, 10, 20)
-        randomized_im = ps.tools.randomize_colors(im=im)
-        assert sp.unique(im).size == sp.unique(randomized_im).size
-        assert sp.all(sp.unique(im) == sp.unique(randomized_im))
-        
-    def test_make_contiguous_size(self):
-        im = sp.random.randint(0, 10, 20)
-        cont_im = ps.tools.make_contiguous(im)
-        assert sp.unique(im).size == sp.unique(cont_im).size
-    
-    def test_make_contiguous_contiguity(self):
-        im = sp.random.randint(0, 10, 20)
-        cont_im = ps.tools.make_contiguous(im)
-        assert sp.all(sp.arange(sp.unique(im).size) == sp.unique(cont_im))
-    
+    def test_sem(self):
+        im = ps.generators.blobs(shape=200)
+        sem = ps.visualization.sem(im)
+        assert sem.ndim == 2
