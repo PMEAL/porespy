d265653aa776b2e3e6b0bfc2945af20d589c0196
Tom Tranter <t.g.tranter@uwaterloo.ca>
2017-09-07 14:11:49 -0400

    Add export to_palabos func + test

--- porespy/export/__funcs__.py
+++ porespy/export/__funcs__.py
@@ -1,4 +1,5 @@
-import scipy as sp
+import numpy as np
+import scipy.ndimage as nd
 
 
 def to_vtk(im, filename):
@@ -8,3 +9,47 @@ def to_vtk(im, filename):
 
     """
     pass
+
+
+def to_palabos(im, filename, solid=0):
+    r"""
+    Converts an ND-array image to a text file that Palabos can read in as a
+    geometry for Lattice Boltzmann simulations. Uses a Euclidean distance
+    transform to identify solid voxels neighboring fluid voxels and labels
+    them as the interface.
+
+    Parameters
+    ----------
+    im : ND-array
+        The image of the porous material
+
+    filename : string
+        Path to output file
+
+    solid : int
+        The value of the solid voxels in the image used to convert image to
+        binary with all other voxels assumed to be fluid.
+
+    Output
+    -------
+    File produced contains 3 values: 2 = Solid, 1 = Interface, 0 = Pore
+
+    """
+    # Create binary image for fluid and solid phases
+    bin_im = im == solid
+    # Transform to integer for distance transform
+    bin_im = bin_im.astype(int)
+    # Distance Transform computes Euclidean distance in lattice units to
+    # Nearest fluid for every solid voxel
+    dt = nd.distance_transform_edt(bin_im)
+    dt[dt > np.sqrt(2)] = 2
+    dt[(dt > 0)*(dt <= np.sqrt(2))] = 1
+    dt = dt.astype(int)
+    # Write out data
+    x, y, z = np.shape(dt)
+    f = open(filename, 'w')
+    for k in range(z):
+        for j in range(y):
+            for i in range(x):
+                f.write(str(dt[i, j, k])+'\n')
+    f.close()
--- porespy/export/__init__.py
+++ porespy/export/__init__.py
@@ -1,1 +1,2 @@
 from .__funcs__ import to_vtk
+from .__funcs__ import to_palabos
