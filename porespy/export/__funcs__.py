16a07df8302f18eef0c5848d5596b816b3971282
DESKTOP-AMEUQVB\mdrkok <mdrkok@gmail.com>
2017-09-14 11:30:13 -0400

    changed the function  name to_vtk and added documentation

--- porespy/export/__funcs__.py
+++ porespy/export/__funcs__.py
@@ -4,41 +4,61 @@ from porespy.export.evtk import hl as bp
 import scipy.ndimage as nd
 
 
-def vox2vtk(im, path='./voxvtk', divide=False, downsample=False, voxel_size=1):
-    im = im.astype(np.int)
+def to_vtk(im, path='./voxvtk', divide=False, downsample=False, voxel_size=1, vox=False):
+    r"""
+    Wrapper for the pyevtk
+    Copyright 2010 - 2016 Paulo A. Herrera. All rights reserved. (see /evtk
+    folder for complete license information)
+    
+    Parameters
+    ----------
+    im : 3D image
+        The image of the porous material
+        
+    path : string
+        Path to output file
+        
+    divide : bool
+        vtk files can get very large, this option allows you for two output 
+        files, divided at z = half. This allows for large data sets to be 
+        imaged without loss of information
+        
+    downsample : bool
+        very large images acan be downsampled to half the size in each 
+        dimension, this doubles the effective voxel size
+        
+    voxel_size : int
+        The side length of the voxels (voxels  are cubic)
+        
+    vox : bool
+        For an image that is binary (1's and 0's) this reduces the file size by 
+        using int8 format (can also be used to reduce file size when accuracy 
+        is not necessary ie: just visulization)
+    
+    Output
+    ------
+    File: vtk, vtp or vti file that can opened in paraview
+    """
+    if vox == True:
+        im = im.astype(np.int8)
     vs = voxel_size
     if divide == True:
         split = np.round(im.shape[2]/2).astype(np.int)
         im1 = im[:, :, 0:split]
         im2 = im[:, :, split:]
-        bp.imageToVTK(path+'1', cellData={'vox': np.ascontiguousarray(im1)},
+        bp.imageToVTK(path+'1', cellData={'im': np.ascontiguousarray(im1)},
                       spacing=(vs, vs, vs))
-        bp.imageToVTK(path+'2', origin=(0.0, 0.0, split),
-                      cellData={'vox': np.ascontiguousarray(im2)},
+        bp.imageToVTK(path+'2', origin=(0.0, 0.0, split*vs),
+                      cellData={'im': np.ascontiguousarray(im2)},
                       spacing=(vs, vs, vs))
     elif downsample == True:
         im = spim.interpolation.zoom(im, zoom=0.5, order=0)
-        bp.imageToVTK(path, cellData={'vox': np.ascontiguousarray(im)},
+        bp.imageToVTK(path, cellData={'im': np.ascontiguousarray(im)},
                       spacing=(vs, vs, vs))
     else:
-        bp.imageToVTK(path, cellData={'vox': np.ascontiguousarray(im)},
-                      spacing=(vs, vs, vs))
+        bp.imageToVTK(path, cellData={'im': np.ascontiguousarray(im)},
+                      spacing=(2*vs, 2*vs, 2*vs))
             
-    
-def im2vtk(im, path='./imvtk', divide=False, downsample=False):
-    if divide == True:
-        split = np.round(im.shape[2]/2).astype(np.int)
-        im1 = im[:, :, 0:split]
-        im2 = im[:, :, split:]
-        bp.imageToVTK(path+'1', cellData={'im': np.ascontiguousarray(im1)})
-        bp.imageToVTK(path+'2', origin=(0.0, 0.0, split), cellData={'im': np.ascontiguousarray(im2)})
-    elif downsample == True:
-        im = spim.interpolation.zoom(im, zoom=0.5, order=0)
-        bp.imageToVTK(path, cellData={'im': np.ascontiguousarray(im)})
-    else:
-        bp.imageToVTK(path, cellData={'im': np.ascontiguousarray(im)})
-
-
 def to_palabos(im, filename, solid=0):
     r"""
     Converts an ND-array image to a text file that Palabos can read in as a
