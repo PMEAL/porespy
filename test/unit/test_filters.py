af463ad130aadf812868a0f19127fd397793c8f6
Jeff Gostick <jeff@gostick.ca>
2017-09-27 20:27:30 -0400

    Added a few more test, added codecov to travis build, and removed automatic deploy

--- test/unit/test_filters.py
+++ test/unit/test_filters.py
@@ -1,5 +1,5 @@
 import porespy as ps
-from porespy import filters
+import pytest
 import scipy as sp
 import scipy.ndimage as spim
 
@@ -8,6 +8,7 @@ class FilterTest():
     def setup_class(self):
         sp.random.seed(0)
         self.im = ps.generators.blobs(shape=[100, 100, 100], blobiness=2)
+        self.im_dt = spim.distance_transform_edt(self.im)
 
     def test_apply_chords_axis0(self):
         c = ps.filters.apply_chords(im=self.im, spacing=0, axis=0)
@@ -22,12 +23,41 @@ class FilterTest():
         assert c.sum() == 25840
 
     def test_apply_chords_with_spacing(self):
+        with pytest.raises(Exception):
+            c = ps.filters.apply_chords(im=self.im, spacing=-1)
         c = ps.filters.apply_chords(im=self.im, spacing=1)
         assert c.sum() == 11367
 
     def test_apply_chords_without_trimming(self):
         c = ps.filters.apply_chords(im=self.im, trim_edges=False)
         assert c.sum() == 31215
+
+    def test_apply_chords3D(self):
+        c = ps.filters.apply_chords_3D(im=self.im)
+        assert c.sum() == 162885
+        assert sp.all(sp.unique(c) == [0, 1, 2, 3])
+
+    def test_apply_chords3D_with_spacing(self):
+        with pytest.raises(Exception):
+            c = ps.filters.apply_chords(im=self.im, spacing=-1)
+        c = ps.filters.apply_chords_3D(im=self.im, spacing=1)
+        assert c.sum() == 74250
+
+    def test_apply_chords3D_without_trimming(self):
+        c = ps.filters.apply_chords_3D(im=self.im, trim_edges=False)
+        assert c.sum() == 187576
+
+    def test_flood_size(self):
+        m = ps.filters.flood(im=self.im[:, :, 50:53], mode='size')
+        s = sp.unique(m)
+        assert len(s) == 19
+        assert max(s) == 13316
+
+    def test_flood_max(self):
+        dt = spim.distance_transform_edt(self.im[:, :, 50:53])
+        m = ps.filters.flood(im=dt, mode='max')
+        s = sp.unique(m)
+
 
 
 if __name__ == '__main__':
@@ -38,3 +68,7 @@ if __name__ == '__main__':
     t.test_apply_chords_axis2()
     t.test_apply_chords_with_spacing()
     t.test_apply_chords_without_trimming()
+    t.test_apply_chords3D()
+    t.test_apply_chords3D_with_spacing()
+    t.test_flood_size()
+    self = t
