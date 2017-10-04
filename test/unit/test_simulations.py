2ff00ddf2f10c72f84e2f569bec8319bbd81978a
Tom Tranter <t.g.tranter@uwaterloo.ca>
2017-10-04 18:59:00 -0400

    Add tests to porosimetry and return hist data for plot_size_histogram function

--- test/unit/test_simulations.py
+++ test/unit/test_simulations.py
@@ -1,15 +1,32 @@
 import porespy as ps
 import scipy as sp
+import matplotlib.pyplot as plt
 
 
 class SimulationTest():
     def setup_class(self):
-        pass
+        self.im = ps.generators.overlapping_spheres(shape=[300, 300],
+                                                    radius=5,
+                                                    porosity=0.5)
+        self.mip = ps.simulations.Porosimetry(self.im)
 
     def test_porosimetry(self):
-        im = ps.generators.overlapping_spheres(shape=[300, 300],
-                                               radius=5,
-                                               porosity=0.5)
-        mip = ps.simulations.Porosimetry(im)
-        mip.run()
-        assert mip.result.dtype == float
+        self.mip.run()
+        assert self.mip.result.dtype == float
+
+    def test_plot_drainage_curve(self):
+        fig = self.mip.plot_drainage_curve()
+        ax = fig.get_axes()[0]
+        line = ax.lines[0]
+        assert line.get_ydata()[0] == 1.0
+
+    def plot_size_histogram(self):
+        fig, counts, bins, bars = self.mip.plot_size_histogram()
+        assert sp.sum(counts) == int(sp.sum(self.mip.result > 0))
+
+if __name__ == '__main__':
+    t = SimulationTest()
+    t.setup_class()
+    t.test_porosimetry()
+    t.test_plot_drainage_curve()
+    t.plot_size_histogram()
