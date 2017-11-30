2a80bd5b82daa899f984127db8516d517b77a6ce
DESKTOP-AMEUQVB\mdrkok <mdrkok@gmail.com>
2017-11-30 16:30:49 -0500

    changing to 'reflect' mode when downsampling, there was a small issue using 'constant'

--- porespy/export/__funcs__.py
+++ porespy/export/__funcs__.py
@@ -53,7 +53,7 @@ def to_vtk(im, path='./voxvtk', divide=False, downsample=False, voxel_size=1,
                       cellData={'im': np.ascontiguousarray(im2)},
                       spacing=(vs, vs, vs))
     elif downsample:
-        im = spim.interpolation.zoom(im, zoom=0.5, order=0)
+        im = spim.interpolation.zoom(im, zoom=0.5, order=0, mode='reflect')
         bp.imageToVTK(path, cellData={'im': np.ascontiguousarray(im)},
                       spacing=(2*vs, 2*vs, 2*vs))
     else:
