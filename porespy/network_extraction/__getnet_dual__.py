e7404f821272ed667b8fe424fb7c45ad1dcd047b
WIN-VUDEO78EP4I\Administrator <zohaib_atiq@yahoo.com>
2017-12-20 17:13:39 -0800

    Adding more information in getnet_dual

--- porespy/network_extraction/__getnet_dual__.py
+++ porespy/network_extraction/__getnet_dual__.py
@@ -1,13 +1,10 @@
 import scipy as sp
 import scipy.ndimage as spim
-from scipy.spatial.distance import cdist
-from porespy.tools import extend_slice
-from tqdm import tqdm
 from porespy.network_extraction import extract_pore_network
 
 
-def extract_dual_network(im, pore_regions=None, solid_regions=None,
-                         voxel_size=1):
+def extract_dual_network(im, pore_regions, solid_regions,
+                         pore_dt=None, solid_dt=None, voxel_size=1):
 
     r"""
     Analyzes an image that has been partitioned into pore and solid regions
@@ -28,6 +25,14 @@ def extract_dual_network(im, pore_regions=None, solid_regions=None,
         A ND-array the with regions belonging to each peak labelled in
         solid phase.
 
+    pore_dt : ND-array
+        The distance transform of the pore space.  If not given it will be
+        calculated, but it can save time to provide one if available.
+
+    solid_dt : ND-array
+        The distance transform of the solid space.  If not given it will be
+        calculated, but it can save time to provide one if available.
+
     voxel_size : scalar
         The resolution of the image, expressed as the length of one side of a
         voxel, so the volume of a voxel would be **voxel_size**-cubed.  The
@@ -41,19 +46,26 @@ def extract_dual_network(im, pore_regions=None, solid_regions=None,
     convention (i.e. 'pore.coords', 'throat.conns') so it may be converted
     directly to an OpenPNM network object using the ``update`` command.
     """
+
+    # Calculates distance transform of pore and solid phase if not provided
+    if pore_dt is None:
+        pore_dt = spim.distance_transform_edt(im)
+    if solid_dt is None:
+        solid_dt = spim.distance_transform_edt(~im)
+    dt = pore_dt + solid_dt
+
+    # Calculates combined pore and solid regions for dual network extraction
     pore_region = pore_regions*im
     solid_region = solid_regions*~im
     solid_num = sp.amax(pore_regions)
-    solid_mask = solid_region > 0
     solid_region = solid_region + solid_num
-    solid_region = solid_region * solid_mask
-    pore_dt = spim.distance_transform_edt(pore_region > 0)
-    pore_dt = spim.gaussian_filter(pore_dt, sigma=0.4)
-    solid_dt = spim.distance_transform_edt(solid_region > 0)
-    solid_dt = spim.gaussian_filter(solid_dt, sigma=0.4)
-    dt = pore_dt + solid_dt
+    solid_region = solid_region * ~im
     regions = pore_region + solid_region
+
+    # Extract pore,solid and throat information from image
     net = extract_pore_network(im=regions, dt=dt, voxel_size=voxel_size)
+
+    # Calculates chunk of solid volume connected with pores and vice versa
     p_on_s = pore_regions*(~im)    # Expose pores labels on solid
     s_on_p = (solid_regions + solid_num) * im  # Expose solid labels on pores
     pv = sp.unique(p_on_s, return_counts=True)[1]
@@ -61,6 +73,8 @@ def extract_dual_network(im, pore_regions=None, solid_regions=None,
     sv = sp.unique(s_on_p, return_counts=True)[1]
     sv = sv[1:]
     p_solid_volume = sp.concatenate((pv, sv))
+
+    # Find pore to pore, pore to solid and solid to solid throat conns
     loc1 = net['throat.conns'][:, 0] < solid_num
     loc2 = net['throat.conns'][:, 1] >= solid_num
     pore_solid_labels = loc1 * loc2
@@ -68,6 +82,8 @@ def extract_dual_network(im, pore_regions=None, solid_regions=None,
     solid_solid_labels = loc3 * loc2
     loc4 = net['throat.conns'][:, 1] < solid_num
     pore_pore_labels = loc1 * loc4
+
+    # Calculates pore surface area that connects with solid and vice versa
     p_conns = net['throat.conns'][:, 0][pore_solid_labels]
     ps = net['throat.area'][pore_solid_labels]
     p_sa = sp.bincount(p_conns, ps)
@@ -76,10 +92,12 @@ def extract_dual_network(im, pore_regions=None, solid_regions=None,
     s_sa = sp.trim_zeros(s_sa)
     p_solid_surf = sp.concatenate((p_sa, s_sa))
 
+    # Adding additional information for dual network dictionary
     net['pore.solid_volume'] = p_solid_volume * voxel_size**3
     net['pore.solid_surface_area'] = p_solid_surf * voxel_size**2
-    net['pore.pore_labels'] = pore_pore_labels
-    net['pore.solid_labels'] = pore_solid_labels
-    net['pore.solid_solid_labels'] = solid_solid_labels
-
+    net['throat.pore_pore_labels'] = pore_pore_labels
+    net['throat.pore_solid_labels'] = pore_solid_labels
+    net['throat.solid_solid_labels'] = solid_solid_labels
+    net['pore.pore_labels'] = net['pore.label'] <= solid_num
+    net['pore.solid_labels'] = net['pore.label'] > solid_num
     return net
