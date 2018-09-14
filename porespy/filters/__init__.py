9331d4f8ba5c005a9b71e1aa8cf4b4af50ef7526
jgostick <jeff@gostick.ca>
2018-09-14 13:29:47 -0400

    Moving things from filters to tools

--- porespy/filters/__init__.py
+++ porespy/filters/__init__.py
@@ -12,7 +12,6 @@ structural characteristics, such as pore sizes.
 .. autofunction:: find_disconnected_voxels
 .. autofunction:: flood
 .. autofunction:: local_thickness
-.. autofunction:: norm_to_uniform
 .. autofunction:: porosimetry
 .. autofunction:: trim_extrema
 .. autofunction:: trim_floating_solid
@@ -28,7 +27,6 @@ from .__funcs__ import fill_blind_pores
 from .__funcs__ import find_disconnected_voxels
 from .__funcs__ import flood
 from .__funcs__ import local_thickness
-from .__funcs__ import norm_to_uniform
 from .__funcs__ import porosimetry
 from .__funcs__ import trim_extrema
 from .__funcs__ import trim_floating_solid
