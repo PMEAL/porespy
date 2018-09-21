e8325c085b095c0001f0befcdd851d30460482ae
jgostick <jeff@gostick.ca>
2018-09-21 18:24:20 -0400

    improving output of chord_length_dist, using general _histogram func

--- porespy/metrics/__init__.py
+++ porespy/metrics/__init__.py
@@ -7,7 +7,7 @@ A suite of tools for determine key metrics about an image.  Typically these are
 applied to an image after applying a filter, but a few functions can be applied
 directly to the binary image.
 
-.. autofunction:: chord_length_counts
+.. autofunction:: chord_counts
 .. autofunction:: chord_length_distribution
 .. autofunction:: pore_size_distribution
 .. autofunction:: porosity
@@ -22,7 +22,7 @@ directly to the binary image.
 
 """
 
-from .__funcs__ import chord_length_counts
+from .__funcs__ import chord_counts
 from .__funcs__ import chord_length_distribution
 from .__funcs__ import pore_size_distribution
 from .__funcs__ import radial_density
@@ -34,4 +34,3 @@ from .__funcs__ import two_point_correlation_fft
 from .__regionprops__ import regionprops_3D
 from .__regionprops__ import props_to_DataFrame
 from .__regionprops__ import props_to_image
-from .__funcs__ import cld_helper
