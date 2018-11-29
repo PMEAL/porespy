6e282638291dbf0342d9b4b8f9b6f278fd9d8f63
Tom Tranter <t.g.tranter@gmail.com>
2018-11-29 17:31:34 -0500

    Put examples tests in own file and split up to call each one eplicitly. Pass on snow_advanced as it times out with marching cubes

--- test/test_package.py
+++ test/test_package.py
@@ -1,57 +1,9 @@
 import porespy as ps
 import git
-import os
-import subprocess
-import logging
-
-
-def run_shell_command(command_line_args):
-    try:
-        command_line_process = subprocess.Popen(
-            command_line_args,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
-        )
-
-        process_output, process_error = command_line_process.communicate()
-        print(process_error)
-    except (OSError, subprocess.CalledProcessError) as exception:
-        logging.info('Exception occured: ' + str(exception))
-        logging.info('Subprocess failed')
-        return False
-    else:
-        # no exception was raised
-        logging.info('Subprocess finished')
-    return "Error" not in str(process_error)
-
-
-def _notebook_run(path):
-    """Execute a notebook via nbconvert and collect output.
-       :returns (parsed nb object, execution errors)
-    """
-    dirname, __ = os.path.split(path)
-    args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
-            "--ExecutePreprocessor.timeout=360",
-            "--output", "temp_output.ipynb", path]
-    rc = run_shell_command(args)
-    print(path, rc)
-    print('-'*30)
-    if rc:
-        os.remove(os.path.join(dirname, "temp_output.ipynb"))
-    return rc
-
-
-def test_ipynb():
-    rootdir = os.path.split(os.getcwd())[0]
-    for path, subdirs, files in os.walk(rootdir):
-        for name in files:
-            if (name.endswith('.ipynb') and 'checkpoint' not in name):
-                nbook = os.path.join(path, name)
-                rc = _notebook_run(nbook)
-                assert rc
 
 
 class PackageTest():
+
     def setup_class(self):
         pass
 
@@ -64,4 +16,5 @@ class PackageTest():
 
 
 if __name__ == '__main__':
-    test_ipynb()
+    t = PackageTest()
+    t.test_version_number_and_git_tag_agree()
---
+++ test/unit/test_examples.py
@@ -0,0 +1,86 @@
+import os
+import subprocess
+import logging
+
+rootdir = os.path.split(os.path.split(os.getcwd())[0])[0]
+examples_dir = os.path.join(rootdir, 'examples')
+filters_dir = os.path.join(examples_dir, 'filters')
+metrics_dir = os.path.join(examples_dir, 'metrics')
+netex_dir = os.path.join(examples_dir, 'network_extraction')
+
+
+class ExamplesTest():
+
+    def setup_class(self):
+        pass
+
+    def run_shell_command(self, command_line_args):
+        try:
+            command_line_process = subprocess.Popen(
+                command_line_args,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
+            process_output, process_error = command_line_process.communicate()
+#            print(process_error)
+        except (OSError, subprocess.CalledProcessError) as exception:
+            logging.info('Exception occured: ' + str(exception))
+            logging.info('Subprocess failed')
+            return False
+        else:
+            # no exception was raised
+            logging.info('Subprocess finished')
+        return "Error" not in str(process_error)
+
+    def _notebook_run(self, path):
+        """Execute a notebook via nbconvert and collect output.
+           :returns (parsed nb object, execution errors)
+        """
+        dirname, __ = os.path.split(path)
+        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+                "--ExecutePreprocessor.timeout=360",
+                "--output", "temp_output.ipynb", path]
+        rc = self.run_shell_command(args)
+        print(path, rc)
+        print('-'*30)
+        if rc:
+            os.remove(os.path.join(dirname, "temp_output.ipynb"))
+        return rc
+
+    def test_filters_porosimetry(self):
+        nbook = os.path.join(filters_dir, 'porosimetry.ipynb')
+        rc = self._notebook_run(nbook)
+        assert rc
+
+    def test_metrics_chord_length_distribution(self):
+        nbook = os.path.join(metrics_dir, 'chord_length_distribution.ipynb')
+        rc = self._notebook_run(nbook)
+        assert rc
+
+    def test_metrics_regionprops_3d(self):
+        nbook = os.path.join(metrics_dir, 'regionprops_3d.ipynb')
+        rc = self._notebook_run(nbook)
+        assert rc
+
+    def test_metrics_two_point_correlation(self):
+        nbook = os.path.join(metrics_dir, 'two_point_correlation.ipynb')
+        rc = self._notebook_run(nbook)
+        assert rc
+
+    def test_network_extraction_snow_basic(self):
+        nbook = os.path.join(netex_dir, 'snow_basic.ipynb')
+        rc = self._notebook_run(nbook)
+        assert rc
+
+    def test_network_extraction_snow_advanced(self):
+        pass
+
+
+if __name__ == '__main__':
+    t = ExamplesTest()
+    self = t
+    t.setup_class()
+    for item in t.__dir__():
+        if item.startswith('test'):
+            print('running test: '+item)
+            t.__getattribute__(item)()
