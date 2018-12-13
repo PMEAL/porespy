34cfbb1bb28ec272f56079941fe06b06389e623d
Unknown <zohaib_atiq@yahoo.com>
2018-12-13 15:43:44 -0500

    more changes

--- porespy/network_extraction/__snow_dual__.py
+++ porespy/network_extraction/__snow_dual__.py
@@ -70,7 +70,8 @@ def snow_dual(im, voxel_size=1,
     # Get combined peaks
     solid_peaks_num =  sp.amax(pore_regions.peaks)
     s_peaks = solid_regions.peaks
-    solid_peaks = (s_peaks + solid_peaks_num) * (s_peaks != solid_peaks_num)
+    solid_peaks = s_peaks + solid_peaks_num * (s_peaks != solid_peaks_num)
+    solid_peaks = solid_peaks * (solid_peaks != solid_peaks_num)
     peaks = solid_peaks + pore_regions.peaks
     # -------------------------------------------------------------------------
     # Combined Distance transform of two phases.
