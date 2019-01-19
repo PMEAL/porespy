7a57229aea2e684b1118c556a03c6dda082b8d22
Amin Sadeghi <amin.sadeghi@live.com>
2019-01-19 09:58:54 -0500

    PEP8 fix, now ignores W503 (line break before operator)

--- test/unit/test_network_extraction.py
+++ test/unit/test_network_extraction.py
@@ -138,21 +138,18 @@ class NetExtractTest():
         net3 = ps.network_extraction.regions_to_network(im=pore_map3,
                                                         dt=snow_out3.dt,
                                                         voxel_size=1)
-        assert np.allclose(net1['pore.coords'][:, 0],
-                            net2['pore.coords'][:, 0])
-        assert np.allclose(net1['pore.coords'][:, 1],
-                            net2['pore.coords'][:, 2])
-        assert np.allclose(net1['pore.coords'][:, 0],
-                            net3['pore.coords'][:, 1])
+        assert np.allclose(net1['pore.coords'][:, 0], net2['pore.coords'][:, 0])
+        assert np.allclose(net1['pore.coords'][:, 1], net2['pore.coords'][:, 2])
+        assert np.allclose(net1['pore.coords'][:, 0], net3['pore.coords'][:, 1])
 
     def test_generate_voxel_image(self):
         net = op.network.Cubic(shape=[5, 5, 5])
         geom = op.geometry.StickAndBall(network=net,
                                         pores=net.Ps, throats=net.Ts)
         geom.add_model(propname="pore.volume",
-                        model=op.models.geometry.pore_volume.cube)
+                       model=op.models.geometry.pore_volume.cube)
         geom.add_model(propname="throat.volume",
-                        model=op.models.geometry.throat_volume.cylinder)
+                       model=op.models.geometry.throat_volume.cylinder)
         im = ps.network_extraction.generate_voxel_image(network=net,
                                                         pore_shape="cube",
                                                         throat_shape="cylinder",
