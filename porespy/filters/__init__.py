0e976bacec6e5c7de259bbc8a6fb2f7572eb4de1
Jeff Gostick <jeff@gostick.ca>
2019-02-02 20:08:42 -0500

    pep8 fix

--- porespy/filters/__init__.py
+++ porespy/filters/__init__.py
@@ -61,7 +61,6 @@ from .__funcs__ import find_disconnected_voxels
 from .__funcs__ import find_dt_artifacts
 from .__funcs__ import find_peaks
 from .__funcs__ import flood
-#from .__funcs__ import hold_peaks
 from .__funcs__ import local_thickness
 from .__funcs__ import porosimetry
 from .__funcs__ import reduce_peaks
