a2a3ec370feaf31d5ee626e419909116332c4aae
Jeff Gostick <jeff@gostick.ca>
2019-02-06 11:19:18 -0500

    removing simulations module

--- porespy/__init__.py
+++ porespy/__init__.py
@@ -83,7 +83,6 @@ from . import metrics
 from . import tools
 from . import filters
 from . import generators
-from . import simulations
 from . import network_extraction
 from . import visualization
 from . import io
