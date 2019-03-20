5fec624554574a950907a92df0973ac7ecda03b2
tomtranter <t.g.tranter@gmail.com>
2019-03-20 11:22:54 +0000

    Undo testing one specific test (committed by mistake after debug)

--- test/unit/test_examples.py
+++ test/unit/test_examples.py
@@ -83,6 +83,6 @@ if __name__ == '__main__':
     self = t
     t.setup_class()
     for item in t.__dir__():
-        if item.startswith('test_network_extraction_snow_advanced'):
+        if item.startswith('test'):
             print('running test: '+item)
             t.__getattribute__(item)()
