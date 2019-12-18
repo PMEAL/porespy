cb5247a332bbf619070cf16e6809e7585536dd42
jgostick <jgostick@uwaterloo.ca>
2019-12-18 14:29:34 -0500

    Doing mass find-replace on sp. to np.

--- porespy/visualization/__views__.py
+++ porespy/visualization/__views__.py
@@ -1,4 +1,4 @@
-import scipy as sp
+import numpy as np
 import scipy.ndimage as spim
 # import matplotlib.pyplot as plt
 # from mpl_toolkits.mplot3d.art3d import Poly3DCollection
@@ -27,14 +27,14 @@ def show_3D(im):
     so inverts the image to show the solid material.
 
     """
-    im = ~sp.copy(im)
+    im = ~np.copy(im)
     if im.ndim < 3:
         raise Exception('show_3D only applies to 3D images')
     im = spim.rotate(input=im, angle=22.5, axes=[0, 1], order=0)
     im = spim.rotate(input=im, angle=45, axes=[2, 1], order=0)
     im = spim.rotate(input=im, angle=-17, axes=[0, 1], order=0, reshape=False)
     mask = im != 0
-    view = sp.where(mask.any(axis=2), mask.argmax(axis=2), 0)
+    view = np.where(mask.any(axis=2), mask.argmax(axis=2), 0)
     view = view.max() - view
     f = view.max()/5
     view[view == view.max()] = -f
@@ -58,18 +58,18 @@ def show_planes(im):
         ``matplotlib.pyplot.imshow``.
 
     """
-    if sp.squeeze(im.ndim) < 3:
+    if np.squeeze(im.ndim) < 3:
         raise Exception('This view is only necessary for 3D images')
-    x, y, z = (sp.array(im.shape)/2).astype(int)
+    x, y, z = (np.array(im.shape)/2).astype(int)
     im_xy = im[:, :, z]
     im_xz = im[:, y, :]
-    im_yz = sp.rot90(im[x, :, :])
+    im_yz = np.rot90(im[x, :, :])
 
     new_x = im_xy.shape[0] + im_yz.shape[0] + 10
 
     new_y = im_xy.shape[1] + im_xz.shape[1] + 10
 
-    new_im = sp.zeros([new_x + 20, new_y + 20], dtype=im.dtype)
+    new_im = np.zeros([new_x + 20, new_y + 20], dtype=im.dtype)
 
     # Add xy image to upper left corner
     new_im[10:im_xy.shape[0]+10,
@@ -107,15 +107,15 @@ def sem(im, direction='X'):
         A 2D greyscale image suitable for use in matplotlib\'s ```imshow```
         function.
     """
-    im = sp.array(~im, dtype=int)
+    im = np.array(~im, dtype=int)
     if direction in ['Y', 'y']:
-        im = sp.transpose(im, axes=[1, 0, 2])
+        im = np.transpose(im, axes=[1, 0, 2])
     if direction in ['Z', 'z']:
-        im = sp.transpose(im, axes=[2, 1, 0])
+        im = np.transpose(im, axes=[2, 1, 0])
     t = im.shape[0]
-    depth = sp.reshape(sp.arange(0, t), [t, 1, 1])
+    depth = np.reshape(np.arange(0, t), [t, 1, 1])
     im = im*depth
-    im = sp.amax(im, axis=0)
+    im = np.amax(im, axis=0)
     return im
 
 
@@ -142,10 +142,10 @@ def xray(im, direction='X'):
         A 2D greyscale image suitable for use in matplotlib\'s ```imshow```
         function.
     """
-    im = sp.array(~im, dtype=int)
+    im = np.array(~im, dtype=int)
     if direction in ['Y', 'y']:
-        im = sp.transpose(im, axes=[1, 0, 2])
+        im = np.transpose(im, axes=[1, 0, 2])
     if direction in ['Z', 'z']:
-        im = sp.transpose(im, axes=[2, 1, 0])
-    im = sp.sum(im, axis=0)
+        im = np.transpose(im, axes=[2, 1, 0])
+    im = np.sum(im, axis=0)
     return im
