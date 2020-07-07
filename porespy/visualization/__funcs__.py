8075f23dd42a8a70a40fff3ff7d1f16bdb63a5b4
jgostick <jgostick@uwaterloo.ca>
2020-07-07 00:04:50 -0400

    Added satn_to_movie func to vis

--- porespy/visualization/__funcs__.py
+++ porespy/visualization/__funcs__.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
 
 
 def set_mpl_style():
@@ -35,3 +37,45 @@ def set_mpl_style():
     plt.rc('ytick', **ytick_props)
     plt.rc('legend', **legend_props)
     plt.rc('figure', **figure_props)
+
+
+def satn_to_movie(im, satn, cmap='viridis',
+                  c_under='grey', c_over='white',
+                  v_under=1e-3, v_over=1.0, fps=10, repeat=None):
+    r"""
+
+    Notes
+    -----
+    To save animation as a file use:
+    ``ani.save('image_based_ip.gif', writer='imagemagick', fps=3)``
+    """
+    import matplotlib.pyplot as plt
+    from matplotlib import animation
+    # Define nice color map
+    cmap = plt.cm.get_cmap(name=cmap)
+    cmap.set_over(color=c_over)
+    cmap.set_under(color=c_under)
+
+    # Reduce inv_satn image to limited number of values to speed-up movie
+    target = np.around(satn, decimals=3)
+    seq = np.zeros_like(target)  # Empty image to place frame
+    movie = []  # List to append each frame
+    fig, ax = plt.subplots(1, 1)
+    steps = np.unique(target)[1:]
+    with tqdm(steps) as pbar:
+        for v in steps:
+            pbar.update()
+            seq += v*(target == v)
+            seq[~im] = target.max() + 10
+            frame1 = ax.imshow(seq, vmin=v_under, vmax=v_over,
+                               animated=True, cmap=cmap, origin='xy')
+            movie.append([frame1])
+    if repeat is not None:
+        repeat_delay = repeat/100
+        repeat = True
+    else:
+        repeat = False
+    ani = animation.ArtistAnimation(fig, movie, interval=int(1000/fps),
+                                    blit=True, repeat=repeat,
+                                    repeat_delay=repeat_delay)
+    return ani
