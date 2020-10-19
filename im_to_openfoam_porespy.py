c202749651a614c2071dd53b71079c640f33e403
Mehrez Agnaou <magnaoulab@gmail.com>
2020-10-19 16:28:31 -0400

    move file

--- im_to_openfoam_porespy.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from skimage import io
-import porespy as ps
-
-
-file = './spheres.tif'
-im = io.imread(file, as_gray=False)[0:20, 0:20, 0:50]
-im = ~np.array(im, dtype=bool)
-ps.io.to_openfoam(im, label=True)
