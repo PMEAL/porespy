303e28604d13ec338960dba29daa64203b65ef05
Jeff <jgostick@gmail.com>
2020-10-21 11:33:49 -0400

    Fixing tqdm usage, adding print outs.

--- porespy/generators/__sssp__.py
+++ porespy/generators/__sssp__.py
@@ -4,6 +4,7 @@ import scipy.ndimage as spim
 from skimage.morphology import disk, ball
 from tqdm import tqdm
 
+
 def pseudo_gravity_packing(im, r, max_iter=1000):
     if np.isscalar(r):
         return _monodisperse(im=im, r=r, max_iter=max_iter)
@@ -12,69 +13,79 @@ def pseudo_gravity_packing(im, r, max_iter=1000):
 
 
 def _monodisperse(im, r, max_iter=1000):
+    print('_'*60)
+    print('Adding monodisperse spheres of size ', r)
     if im.ndim == 2:
         strel = disk
     else:
         strel = ball
     sites = ps.tools.fftmorphology(im == 1, strel=strel(r), mode='erosion')
-    for _ in tqdm(range(max_iter)):
-        if im.ndim == 2:
-            x, y = np.where(sites)
-        else:
-            x, y, z = np.where(sites)
-        if len(x) == 0:
-            break
-        options = np.where(x == x.min())[0]
-        if len(options) > 1:
-            choice = np.random.randint(0, len(options)-1)
-        else:
-            choice = 0
-        if im.ndim == 2:
-            cen = np.array([x[options[choice]],
-                            y[options[choice]]])
-        else:
-            cen = np.array([x[options[choice]],
-                            y[options[choice]],
-                            z[options[choice]]])
-        im = ps.tools.insert_sphere(im, c=cen, r=r, v=0)
-        sites = ps.tools.insert_sphere(sites, c=cen, r=2*r, v=0)
+    with tqdm(range(max_iter)) as pbar:
+        for _ in range(max_iter):
+            pbar.update()
+            if im.ndim == 2:
+                x, y = np.where(sites)
+            else:
+                x, y, z = np.where(sites)
+            if len(x) == 0:
+                break
+            options = np.where(x == x.min())[0]
+            if len(options) > 1:
+                choice = np.random.randint(0, len(options)-1)
+            else:
+                choice = 0
+            if im.ndim == 2:
+                cen = np.array([x[options[choice]],
+                                y[options[choice]]])
+            else:
+                cen = np.array([x[options[choice]],
+                                y[options[choice]],
+                                z[options[choice]]])
+            im = ps.tools.insert_sphere(im, c=cen, r=r, v=0)
+            sites = ps.tools.insert_sphere(sites, c=cen, r=2*r, v=0)
+    print('A total of ', _, 'spheres were added')
     im = spim.minimum_filter(input=im, footprint=ball(1))
     return im
 
 def _polydisperse(im, r_min, r_max, max_iter=1000):
+    print('_'*60)
+    print('Adding polydisperse spheres of sizes ', r_min, ' to ', r_max)
     bd = np.zeros_like(im)
     bd[-r_max:, ...] = 1
     if im.ndim == 2:
         strel = disk
     else:
         strel = ball
-    for _ in tqdm(range(max_iter)):
-        r = np.random.randint(r_min, r_max)
-        sites = ps.filters.chunked_func(func=ps.tools.fftmorphology,
-                                        overlap=r, divs=3, cores=None,
-                                        im=im == 0, strel=strel(r),
-                                        mode='erosion')
-        sites = ps.filters.trim_disconnected_blobs(sites, inlets=bd)
-        if im.ndim == 2:
-            x, y = np.where(sites)
-        else:
-            x, y, z = np.where(sites)
-        if len(x) == 0:
-            break
-        options = np.where(x == x.min())[0]
-        if len(options) > 1:
-            choice = np.random.randint(0, len(options)-1)
-        else:
-            choice = 0
-        if im.ndim == 2:
-            cen = np.array([x[options[choice]],
-                            y[options[choice]]])
-        else:
-            cen = np.array([x[options[choice]],
-                            y[options[choice]],
-                            z[options[choice]]])
-        im = ps.tools.insert_sphere(im, c=cen, r=r, v=0)
+    with tqdm(range(max_iter)) as pbar:
+        for _ in range(max_iter):
+            pbar.update()
+            r = np.random.randint(r_min, r_max)
+            sites = ps.filters.chunked_func(func=ps.tools.fftmorphology,
+                                            overlap=r, divs=3, cores=None,
+                                            im=im == 0, strel=strel(r),
+                                            mode='erosion')
+            sites = ps.filters.trim_disconnected_blobs(sites, inlets=bd)
+            if im.ndim == 2:
+                x, y = np.where(sites)
+            else:
+                x, y, z = np.where(sites)
+            if len(x) == 0:
+                break
+            options = np.where(x == x.min())[0]
+            if len(options) > 1:
+                choice = np.random.randint(0, len(options)-1)
+            else:
+                choice = 0
+            if im.ndim == 2:
+                cen = np.array([x[options[choice]],
+                                y[options[choice]]])
+            else:
+                cen = np.array([x[options[choice]],
+                                y[options[choice]],
+                                z[options[choice]]])
+            im = ps.tools.insert_sphere(im, c=cen, r=r, v=0)
     im = spim.minimum_filter(input=im, footprint=ball(1))
+    print('A total of ', _, 'spheres were added')
     return im
 
 # im = np.ones([1000, 250, 250])
