cffe0af2a3d04fc2823c7d9aeca14f4ac0ce862e
jgostick <jgostick@gmail.com>
2021-01-16 12:15:14 -0500

    more tests

--- test/unit/test_filters_ibip.py
+++ test/unit/test_filters_ibip.py
@@ -16,17 +16,21 @@ class IBIPTest():
         bd[:, 0] = True
         self.bd = bd
 
-    def test_ibip(self):
-        inv = ps.filters.ibip(self.im, inlets=self.bd)
-        assert inv.max() == 119
-
-    def test_ibip_w_trapping(self):
+    def sc_lattice_with_trapped_region(self):
         im = np.copy(self.im)
         crds = ps.generators.line_segment([64, 64], [49, 80])
         im[tuple(crds)] = False
         crds = ps.generators.line_segment([30, 64], [49, 80])
         im[tuple(crds)] = False
         im = ~spim.binary_dilation(~im, structure=square(3))
+        return im
+
+    def test_ibip(self):
+        inv = ps.filters.ibip(self.im, inlets=self.bd)
+        assert inv.max() == 119
+
+    def test_ibip_w_trapping(self):
+        im = self.sc_lattice_with_trapped_region()
         inv = ps.filters.ibip(im, inlets=self.bd)
         assert inv.max() == 153
         inv_w_trapping = ps.filters.find_trapped_regions(seq=inv,
@@ -36,8 +40,17 @@ class IBIPTest():
                                                          return_mask=False)
         assert (inv_w_trapping == -1).sum() == 390
 
-
-
+    def test_mio_w_trapping(self):
+        np.random.seed(0)
+        im = ps.generators.overlapping_spheres(shape=[100, 100],
+                                               radius=6, porosity=0.6)
+        bd = np.zeros_like(im)
+        bd[:, 0] = True
+        inv = ps.filters.porosimetry(im, inlets=bd)
+        seq = ps.tools.size_to_seq(inv)
+        inv_w_trapping = ps.filters.find_trapped_regions(seq=seq,
+                                                         return_mask=False)
+        assert (inv_w_trapping == -1).sum() == 236
 
 
 if __name__ == '__main__':
