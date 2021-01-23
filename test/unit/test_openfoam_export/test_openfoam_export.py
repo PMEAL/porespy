8a4a3d7155352d33188e258d8743b4aae59436f8
jgostick <jgostick@gmail.com>
2021-01-22 23:11:46 -0500

    updating test

--- test/unit/test_openfoam_export/test_openfoam_export.py
+++ test/unit/test_openfoam_export/test_openfoam_export.py
@@ -8,7 +8,7 @@ im = io.imread('./image_to_read.tif', as_gray=False)
 im = ~np.array(im, dtype=bool)
 
 # this should generate an "blockMeshDict" file
-ps.io.to_openfoam(im, label=True, path='./openfoam_case/system/')
+ps.io.to_openfoam(im, label_boundaries=True, path='./openfoam_case/system/')
 
 # to generate the mesh, simply run "blockMesh" command from a terminal after
 # doing "cd" to the "openfoam_case"
