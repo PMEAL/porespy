2cb745e0ed3514fc7029c6ea5b25ce2e7425d57c
jgostick <jgostick@gmail.com>
2021-02-13 20:43:45 -0500

    works great

--- porespy/generators/__init__.py
+++ porespy/generators/__init__.py
@@ -53,3 +53,4 @@ from .__imgen__ import polydisperse_spheres
 from .__imgen__ import RSA
 from .__imgen__ import voronoi_edges
 from .__imgen__ import pseudo_gravity_packing
+from .__cylinder__ import cylindrical_plug
