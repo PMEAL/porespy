6833ef70d0f76a7bd82095f1715f438f22b4d69a
Hamed Fathiannasab <hfathian@uwaterloo.ca>
2021-02-16 10:45:01 -0500

    imagej_wrapper test file

---
+++ test/unit/test_imagej.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import pytest
+import numpy as np
+import porespy as ps
+import openpnm as op
+from numpy.testing import assert_allclose
+
+
+class ExportTest():
+
+    def setup_class(self):
+        self.path = os.path.dirname(os.path.abspath(sys.argv[0]))
+    
+    def test_imagej_wrapper(self):
+        img = ps.generators.blobs(shape=[50, 50, 50], porosity=.5, blobiness=2)
+        plgn = ps.imagej.imagej_wrapper(img, 'mean', 'sc.fiji:fiji:2.1.1')
+        assert sum(plgn.shape) == 150
+              
+if __name__ == "__main__":
+    t = ExportTest()
+    self = t
+    t.setup_class()
+    for item in t.__dir__():
+        if item.startswith("test"):
+            print(f"Running test: {item}")
+            t.__getattribute__(item)()
+
