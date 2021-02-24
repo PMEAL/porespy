01a51e74f4f1e059b2d1118a0660340ec1ffbc5b
Hamed Fathiannasab <hfathian@uwaterloo.ca>
2021-02-24 18:46:03 -0500

    Update init file

--- porespy/imagej/__init__.py
+++ porespy/imagej/__init__.py
@@ -9,10 +9,13 @@ imagej
 .. autosummary::
 
     porespy.imagej.imagej_wrapper
+    porespy.imagej.imagej_plugin
 
 
 .. autofunction:: imagej_wrapper
+.. autofunction:: imagej_plugin
 
 """
 
 from .__funcs__ import imagej_wrapper
+from .__funcs__ import imagej_plugin
