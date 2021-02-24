8141c344b26e093d3a893f5a60f5b2d6eac8ba4e
madeline_am <mamhayte@uwaterloo.ca>
2021-02-24 16:49:45 -0500

    adding unpad filter [ci skip]
    [ci skip]

---
+++ examples/filters/unpad.py
@@ -0,0 +1,50 @@
+import porespy as ps
+import numpy as np
+import matplotlib.pyplot as plt
+from porespy.tools import get_border, extend_slice, extract_subsection
+
+
+def unpad(im, pad_width):
+    r"""
+    removes padding from an image then extracts the result corresponding to the original image shape.
+    Parameters
+    ----------
+    im : ND-image
+        The image to which ``func`` should be applied
+    pad_width : int or list of ints
+        The amount of padding on each axis.  Refer to ``numpy.pad``
+        documentation for more details.
+    Notes
+    -----
+    A use case for this is when using ``skimage.morphology.skeletonize_3d``
+    to ensure that the skeleton extends beyond the edges of the image.
+    """
+    padded = np.pad(im, pad_width=pad_width,
+                    mode='constant', constant_values=1)
+    
+    if type(pad_width) == int:
+        new_pad_width = []
+        for r in range (0,len(im.shape)):
+            new_pad_width.append(pad_width)
+        pad_width = new_pad_width
+    
+    if type(pad_width) == list:
+        pad_width = np.asarray(pad_width)
+        shape = im.shape - pad_width[0] - pad_width[1]
+       
+        
+    difference = (pad_width[0]-pad_width[1])
+    if shape[0] < 1:
+        shape = np.array(im.shape) * shape
+    center = np.array(im.shape)
+    center[0] = shape[0]/2 -difference
+    center[1] = shape[1]/2 -difference
+    s_im = []
+    for dim in range(im.ndim):
+        r = shape[dim] / 2
+        lower_im = pad_width[0]
+        upper_im = shape[dim] + pad_width[0]        
+        s_im.append(slice(int(lower_im), int(upper_im)))
+
+        
+    return im[tuple(s_im)]
