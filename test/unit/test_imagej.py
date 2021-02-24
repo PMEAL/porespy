b63db79fe980c9c584a3e6fb2ad77770f91f195c
Hamed Fathiannasab <hfathian@uwaterloo.ca>
2021-02-24 18:46:40 -0500

    Generic ImageJ test function

--- test/unit/test_imagej.py
+++ test/unit/test_imagej.py
@@ -14,8 +14,13 @@ class ImageJTest:
             img = ps.generators.blobs(shape=[50, 50, 50], porosity=.5, blobiness=2)
             plgn = ps.imagej.imagej_wrapper(img, 'mean', 'sc.fiji:fiji:2.1.1')
             assert sum(plgn.shape) == 150
-
-
+    
+    def test_imagej_plugin(self):
+        if system() == 'Windows':
+            img = ps.generators.blobs(shape=[50, 50, 50], porosity=.5, blobiness=2)
+            test = ps.imagej.imagej_plugin(img, path='sc.fiji:fiji:2.1.1', plugin_name='Mean 3D...', args={'options':'True'})
+            assert sum(test.shape) == 150
+            
 if __name__ == "__main__":
     t = ImageJTest()
     self = t
