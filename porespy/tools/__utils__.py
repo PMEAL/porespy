e97fe702018e863d2649574b3d9c2be24ae4f52c
Amin Sadeghi <amin.sadeghi@live.com>
2021-03-10 20:13:13 -0500

    Added helper method for configuring loguru + minor refactor

--- porespy/tools/__utils__.py
+++ porespy/tools/__utils__.py
@@ -3,30 +3,38 @@ import sys
 import importlib
 from dataclasses import dataclass
 from loguru import logger
+from tqdm import tqdm
 
 
 @dataclass
-class Settings:
+class Settings:  # pragma: no cover
     r"""
     A dataclass for use at the module level to store settings.  This class
-    is defined as a Singleton so now matter how or where it gets instantiated
-    the same object is returned, containing all existing settings.
+    is defined as a Singleton so now matter how or where it gets
+    instantiated the same object is returned, containing all existing
+    settings.
 
     Parameters
     ----------
     notebook : boolean
         Is automatically determined upon initialization of PoreSpy, and is
-        ``True`` if running within a Jupyter notebook and ``False`` otherwise.
-        This is used by the ``porespy.tools.get_tqdm`` function to determine
-        whether a standard or a notebook version of the progress bar should
-        be used.
+        ``True`` if running within a Jupyter notebook and ``False``
+        otherwise. This is used by the ``porespy.tools.get_tqdm`` function
+        to determine whether a standard or a notebook version of the
+        progress bar should be used.
     tqdm : dict
         This dictionary is passed directly to the the ``tqdm`` function
-        throughout PoreSpy (i.e. ``for i in tqdm(range(N), **settings.tqdm)``).
-        To see a list of available options visit the tqdm website.  Probably
-        the most important is ``'disable'`` which when set to ``True`` will
-        silence the progress bars.  It's also possible to adjust the formatting
-        such as ``'colour'`` and ``'ncols'``, which controls width.
+        throughout PoreSpy (``for i in tqdm(range(N), **settings.tqdm)``).
+        To see a list of available options visit the tqdm website.
+        Probably the most important is ``'disable'`` which when set to
+        ``True`` will silence the progress bars.  It's also possible to
+        adjust the formatting such as ``'colour'`` and ``'ncols'``, which
+        controls width.
+    logger_fmt : str
+        luguru-compatible format used to format the logger messages.
+    loglevel : str
+        Determines what messages to get printed in console. Options are:
+        "TRACE", "DEBUG", "INFO", "SUCCESS", "WARNING", "ERROR", "CRITICAL"
 
     """
     __instance__ = None
@@ -35,20 +43,32 @@ class Settings:
     tqdm = {'disable': False,
             'colour': None,
             'ncols': None,
-            'leave': False}
+            'leave': False,
+            'file': sys.stdout}
+    _logger_fmt = '<green>{time:YYYY-MM-DD HH:mm:ss}</green> | ' \
+          '<level>{level: <8}</level> | ' \
+          '<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan>' \
+          '\n--> <level>{message}</level>'
     _loglevel = "INFO"
+
+    @property
+    def logger_fmt(self):
+        return self._logger_fmt
 
     @property
     def loglevel(self):
         return self._loglevel
 
+    @logger_fmt.setter
+    def logger_fmt(self, value):
+        self._logger_fmt = value
+        config_logger(fmt=value, loglevel=self.loglevel)
+
     @loglevel.setter
     def loglevel(self, value):
         self._loglevel = value
         os.environ["LOGURU_LEVEL"] = value
-        # The following lines are necessary for new loglevel to activate
-        logger.remove()
-        logger.add(sys.stderr, level=value)
+        config_logger(fmt=self.logger_fmt, loglevel=value)
 
     def __new__(cls):
         if Settings.__instance__ is None:
@@ -60,7 +80,6 @@ class Settings:
         for item in self.__dir__():
             if not item.startswith('_'):
                 indent = max(indent, len(item) + 1)
-        print(indent)
         s = ''
         for item in self.__dir__():
             if not item.startswith('_'):
@@ -73,9 +92,9 @@ class Settings:
         return s
 
 
-def get_tqdm():
+def get_tqdm():  # pragma: no cover
     r"""
-    Fetches a version of the ``tqdm`` function that depends on the environment
+    Fetches a version of ``tqdm`` function that depends on the environment.
 
     Either text-based for the IPython console or gui-based for Jupyter
     notebooks.
@@ -84,20 +103,20 @@ def get_tqdm():
     -------
     tqdm : function handle
         The function to use when wrapping an iterator (i.e. tqdm(range(n)))
+
     """
-    s = Settings()
-    if s.notebook is True:
+    if Settings().notebook is True:
         tqdm = importlib.import_module('tqdm.notebook')
     else:
         tqdm = importlib.import_module('tqdm')
     return tqdm.tqdm
 
 
-def show_docstring(func):
+def show_docstring(func):  # pragma: no cover
     r"""
-    Fetches the docstring for a function and returns it in markdown format
+    Fetches the docstring for a function and returns it in markdown format.
 
-    Useful for printing in a Jupyter notebook
+    Useful for printing in a Jupyter notebook.
 
     Parameters
     ----------
@@ -107,8 +126,10 @@ def show_docstring(func):
     Returns
     -------
     md : str
-        A text string with the markdown syntax included, suitable for printing in
-        a Jupyter notebook using the ``IPython.display.Markdown`` function.
+        A string with the markdown syntax included, suitable for printing
+        in a Jupyter notebook using the ``IPython.display.Markdown``
+        function.
+
     """
     title = f'---\n ## Documentation for ``{func.__name__}``\n ---\n'
     try:
@@ -117,3 +138,24 @@ def show_docstring(func):
     except ModuleNotFoundError:
         txt = func.__doc__
     return title + txt + '\n---'
+
+
+def config_logger(fmt, loglevel):  # pragma: no cover
+    r"""
+    Configures loguru logger with the given format and log level.
+
+    Parameters
+    ----------
+    fmt : str
+        loguru-compatible format used to format logger messages.
+    loglevel : str
+        Determines what messages to get printed in console. Options are:
+        "TRACE", "DEBUG", "INFO", "SUCCESS", "WARNING", "ERROR", "CRITICAL"
+
+    Returns
+    -------
+    None.
+
+    """
+    logger.remove()
+    logger.add(lambda msg: tqdm.write(msg, end=""), level=loglevel, format=fmt)
