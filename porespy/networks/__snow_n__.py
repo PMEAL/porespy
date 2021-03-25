b11b29b5faf5e62e498689bf00591c0c13669e86
jgostick <jgostick@gmail.com>
2021-03-24 23:53:57 -0400

    starting to renovate snow_n

--- porespy/networks/__snow_n__.py
+++ porespy/networks/__snow_n__.py
@@ -13,8 +13,8 @@ from porespy.metrics import region_surface_areas, region_interface_areas
 def snow_n(im,
            voxel_size=1,
            boundary_faces=['top', 'bottom', 'left', 'right', 'front', 'back'],
-           marching_cubes_area=False,
-           alias=None):
+           accuracy='standard',
+           alias={1: 'void', 2: 'solid'}):
     r"""
     Analyzes an image that has been segemented into N phases and extracts all
     a network for each of the N phases, including geometerical information as
@@ -25,13 +25,11 @@ def snow_n(im,
     im : ND-array
         Image of porous material where each phase is represented by unique
         integer. Phase integer should start from 1 (0 is ignored)
-
     voxel_size : scalar
         The resolution of the image, expressed as the length of one side of a
         voxel, so the volume of a voxel would be **voxel_size**-cubed.  The
         default is 1, which is useful when overlaying the PNM on the original
         image since the scale of the image is always 1 unit lenth per voxel.
-
     boundary_faces : list of strings
         Boundary faces labels are provided to assign hypothetical boundary
         nodes having zero resistance to transport process. For cubical
@@ -39,20 +37,22 @@ def snow_n(im,
         ‘front’ and ‘back’ face labels to assign boundary nodes. If no label is
         assigned then all six faces will be selected as boundary nodes
         automatically which can be trimmed later on based on user requirements.
-
-    marching_cubes_area : bool
-        If ``True`` then the surface area and interfacial area between regions
-        will be calculated using the marching cube algorithm. This is a more
-        accurate representation of area in extracted network, but is quite
-        slow, so it is ``False`` by default.  The default method simply counts
-        voxels so does not correctly account for the voxelated nature of the
-        images.
-
     alias : dict (Optional)
         A dictionary that assigns unique image label to specific phases. For
         example {1: 'Solid'} will show all structural properties associated
         with label 1 as Solid phase properties. If ``None`` then default
         labelling will be used i.e {1: 'Phase1',..}.
+    accuracy : string
+        Controls how accurately certain properties are calculated. Options are:
+
+        'standard' (default)
+            Computes the surface areas and perimeters by simply counting
+            voxels.  This is *much* faster but does not properly account
+            for the rough, voxelated nature of the surfaces.
+        'high'
+            Computes surface areas using the marching cube method, and
+            perimeters using the fast marching method.  These are substantially
+            slower but better account for the voxelated nature of the images.
 
     Returns
     -------
@@ -60,6 +60,7 @@ def snow_n(im,
     network topological information.  The dictionary names use the OpenPNM
     convention (i.e. 'pore.coords', 'throat.conns') so it may be converted
     directly to an OpenPNM network object using the ``update`` command.
+
     """
     # Get alias if provided by user
     alias = _create_alias_map(im, alias=alias)
@@ -81,12 +82,13 @@ def snow_n(im,
     # Extract N phases sites and bond information from image
     net = regions_to_network(im=regions, dt=dt, voxel_size=voxel_size)
     # Extract marching cube surface area and interfacial area of regions
-    if marching_cubes_area:
+    if accuracy == 'high':
         areas = region_surface_areas(regions=regions)
         interface_area = region_interface_areas(regions=regions, areas=areas,
                                                 voxel_size=voxel_size)
         net['pore.surface_area'] = areas * voxel_size ** 2
         net['throat.area'] = interface_area.area
+        marching_cubes_area = True
     # Find interconnection and interfacial area between ith and jth phases
     net = add_phase_interconnections(net=net, snow_partitioning_n=snow,
                                      marching_cubes_area=marching_cubes_area,
