b9992de99d22517b8bbda2a891a716fc580f56a5
jgostick <jgostick@gmail.com>
2021-03-28 16:37:46 -0400

    updating two extraction examples

--- porespy/networks/__getnet__.py
+++ porespy/networks/__getnet__.py
@@ -181,6 +181,8 @@ def regions_to_network(regions, phases=None, voxel_size=1, accuracy='standard'):
     # Define all the fundamental stuff
     net['throat.conns'] = np.array(t_conns)
     net['pore.coords'] = np.array(p_coords)
+    net['pore.all'] = np.ones_like(net['pore.coords'][:, 0], dtype=bool)
+    net['throat.all'] = np.ones_like(net['throat.conns'][:, 0], dtype=bool)
     net['pore.region_label'] = np.array(p_label)
     net['pore.region_volume'] = np.copy(p_volume)*(voxel_size**3)
     net['pore.phase'] = np.array(p_phase, dtype=int)
