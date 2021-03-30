6be5aa41cbf8aaa01de140aa3ea1f1c77466a29e
Amin Sadeghi <amin.sadeghi@live.com>
2021-03-30 13:35:36 -0400

    Modified init file of visualization module for the new documenation format

--- porespy/visualization/__init__.py
+++ porespy/visualization/__init__.py
@@ -8,17 +8,20 @@ Visualization
 This module contains functions for quickly visualizing 3D images in 2D
 views.
 
+.. currentmodule:: porespy
+
 .. autosummary::
+   :template: mybase.rst
    :toctree: generated/
 
-   sem
-   show_planes
-   xray
-   show_3D
-   imshow
-   bar
-   show_mesh
-   set_mpl_style
+   visualization.sem
+   visualization.show_planes
+   visualization.xray
+   visualization.show_3D
+   visualization.imshow
+   visualization.bar
+   visualization.show_mesh
+   visualization.set_mpl_style
 
 """
 
