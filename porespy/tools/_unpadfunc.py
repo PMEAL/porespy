a08985093245eea6250fe91b8216fbe2ee534bb6
madeline-am <madelinesusername@gmail.com>
2021-04-12 09:40:22 -0400

    unpad

--- porespy/tools/_unpadfunc.py
+++ porespy/tools/_unpadfunc.py
@@ -1,12 +1,10 @@
-import porespy as ps
 import numpy as np
-import matplotlib.pyplot as plt
-from porespy.tools import get_border, extend_slice, extract_subsection
-
 
 def unpad(im, pad_width):
     r"""
-    removes padding from an image then extracts the result corresponding to the original image shape.
+    Remove padding from an image then extracts the result corresponding to
+    the original image shape.
+
     Parameters
     ----------
     im : ND-image
@@ -14,18 +12,20 @@ def unpad(im, pad_width):
     pad_width : int or list of ints
         The amount of padding on each axis.  Refer to ``numpy.pad``
         documentation for more details.
+
     Notes
     -----
     A use case for this is when using ``skimage.morphology.skeletonize_3d``
     to ensure that the skeleton extends beyond the edges of the image.
+
     """
-    if type(pad_width) == int:
+    if isinstance(pad_width, int):
         new_pad_width = []
         for r in range(0, len(im.shape)):
             new_pad_width.append(pad_width)
         pad_width = new_pad_width
 
-    if type(pad_width) == list and np.ndim(pad_width) == 1:
+    if isinstance(pad_width, list) and np.ndim(pad_width) == 1:
         pad_width = np.asarray(pad_width)
         shape = im.shape - pad_width[0] - pad_width[1]
 
@@ -37,7 +37,7 @@ def unpad(im, pad_width):
             upper_im = shape[dim] + pad_width[0]
             s_im.append(slice(int(lower_im), int(upper_im)))
 
-    if type(pad_width) == list and np.ndim(pad_width) == 2:
+    if isinstance(pad_width, list) and np.ndim(pad_width) == 2:
         pad_width = np.asarray(pad_width)
         shape = np.asarray(im.shape)
         s_im = []
