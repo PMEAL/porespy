1971f0f81701b4b5df4257021189bb9488a8fd09
Amin Sadeghi <amin.sadeghi@live.com>
2021-04-13 09:30:43 -0400

    Push some trivial change to force rebuild docs

--- example_snow.py
+++ example_snow.py
@@ -1,17 +1,17 @@
+import numpy as np
 import porespy as ps
 import matplotlib.pyplot as plt
 import openpnm as op
 
-# np.random.seed(3)
+
+np.random.seed(3)
 im1 = ps.generators.blobs(shape=[400, 200], porosity=None, blobiness=2) < 0.4
 im2 = ps.generators.blobs(shape=[400, 200], porosity=None, blobiness=2) < 0.7
 phases = im1 + (im2 * ~im1)*2
 # phases = phases > 0
 
-snow_n = ps.networks.snow2(phases,
-                           phase_alias={1: 'solid', 2: 'void'},
-                           boundary_width=5,
-                           return_all=True)
+snow_n = ps.networks.snow2(
+    phases, phase_alias={1: 'solid', 2: 'void'}, boundary_width=5, return_all=True)
 
 fig = plt.figure()
 plt.imshow(snow_n.regions.T)
