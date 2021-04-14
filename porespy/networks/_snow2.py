1a1f381186b1ef13c35d85ac7826a296dbb0885e
Jeff Gostick <jgostick@gmail.com>
2021-04-14 13:49:04 -0400

    Removing option to specify boundary labels (people can do it afterward)

--- porespy/networks/_snow2.py
+++ porespy/networks/_snow2.py
@@ -9,10 +9,8 @@ from collections import namedtuple
 def snow2(phases,
           phase_alias=None,
           boundary_width=3,
-          boundary_labels=None,
           accuracy='standard',
           voxel_size=1,
-          return_all=False,
           parallelization={},):
     r"""
     Applies the SNOW algorithm to each phase indicated in ``phases``.
@@ -33,9 +31,9 @@ def snow2(phases,
         image, ``{1: 'void', 2: 'solid'}`` will result in the labels
         ``'pore.void'`` and ``'pore.solid'``, as well as
         ``'throat.solid_void'``, ``'throat.solid_solid'``, and
-        ``'throat.void_void'``. If not provided, alias is assumed as
+        ``'throat.void_void'``. If not provided, aliases are assumed to be
         ``{1: 'phase1', 2: 'phase2, ...}``.  Phase labels can also be applied
-           afterward using ``label_phases``.
+        afterward using ``label_phases``.
     boundary_width : depends
         Number of voxels to add to the beginning and end of each axis. This
         argument is handled the same as ``pad_width`` in the ``np.pad``
@@ -43,15 +41,9 @@ def snow2(phases,
         each axis. ``[A, B]`` adds A to the beginning of each axis and B to the
         ends.  ``[[A, B], ..., [C, D]]`` adds A to the beginning and B to the
         end of the first axis, and so on. The default is to add 3 voxels on
-        both ends of all axes.
-    boundary_labels : list of lists
-        A 3-element list with each element containing a pair of strings
-        indicating the label to apply to the beginning and end of each axis.
-        For instance, ``[['left', 'right'], ['front', 'back'],
-        ['top', 'bottom']]`` will apply the label ``'left'`` to all pores
-        with the minimum x-coordinate, and ``'right'`` to the pores with the
-        maximum x-coordinate, and so on.  Boundary labels can also be applied
-        afterward using ``label_boundaries``.
+        both ends of all axes.  For each boundary width that is not 0, a label
+        will automatically be applied indicating which end of which axis (i.e.
+        ``'xmin'`` and ``'xmax'``).
     accuracy : string
         Controls how accurately certain properties are calculated during the
         analysis of regions in the ``regions_to_network`` function.
@@ -69,11 +61,6 @@ def snow2(phases,
     voxel_size : scalar (default = 1)
         The resolution of the image, expressed as the length of one side of a
         voxel, so the volume of a voxel would be **voxel_size**-cubed.
-    return_all : boolean
-        If set to ``True`` a named tuple is returned containing the
-        padded ``phases`` image, the pore ``regions``, and a dictionary
-        containing the extracted ``network``. The default is ``False``,
-        so only the network dictionary is returned.
     parallelization : dict
         The arguments for controlling the parallization of the watershed
         function are rolled into this dictionary, otherwise the function
@@ -86,9 +73,7 @@ def snow2(phases,
     Returns
     -------
     network : dict or named-tuple
-        If ``return_all = False`` then a dictionary containing the extracted
-        network data is returned.  If ``return_all = True`` then a
-        *named-tuple* is returned with the padded ``phases`` image, the
+        A *named-tuple* is returned with the padded ``phases`` image, the
         watershed segmentated ``regions``, and the ``network`` dictionary.
 
     References
@@ -144,16 +129,15 @@ def snow2(phases,
         net = label_phases(net, alias=phase_alias)
     # If boundaries were added, label them accordingly
     if np.any(boundary_width):
-        boundary_labels = _parse_boundary_labels(boundary_labels,
-                                                 pad_width=boundary_width)
-        net = label_boundaries(net, labels=boundary_labels)
-    if return_all:  # Collect result in a tuple for return
-        result = namedtuple('snow2', ('network', 'regions', 'phases'))
-        result.network = net
-        result.regions = regions
-        result.phases = phases
-    else:
-        result = net
+        W = boundary_width.flatten()
+        L = ['xmin', 'xmax', 'ymin', 'ymax', 'zmin', 'zmax'][:phases.ndim*2]
+        L = [L[i]*int(W[i] > 0) for i in range(len(L))]
+        L = np.reshape(L, newshape=boundary_width.shape)
+        net = label_boundaries(net, labels=L)
+    result = namedtuple('snow2', ('network', 'regions', 'phases'))
+    result.network = net
+    result.regions = regions
+    result.phases = phases
     return result
 
 
@@ -186,20 +170,3 @@ def _parse_pad_width(pad_width, shape):
     elif (pw.size == 3) and (shape.size == 2):
         raise Exception(f'Not sure how to interpret {pad_width} on a 2D image')
     return pw.squeeze()
-
-
-def _parse_boundary_labels(boundary_labels, pad_width):
-    r"""
-    """
-    if boundary_labels is None:
-        boundary_labels = [['left', 'right'],
-                           ['front', 'back'],
-                           ['bottom', 'top']]
-    bl = []
-    for r in range(pad_width.shape[1]):
-        temp = []
-        for c in [0, 1]:
-            if pad_width[r, c] > 0:
-                temp.append(boundary_labels[r][c])
-        bl.append(temp)
-    return bl
