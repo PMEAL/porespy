55bff04345d86c0700978874c593dfcb45f96dce
Amin Sadeghi <amin.sadeghi@live.com>
2021-06-24 20:23:49 -0400

    Modify unit test for _parse_boundary_width

--- test/unit/test_snow2.py
+++ test/unit/test_snow2.py
@@ -1,12 +1,9 @@
-import os
 import pytest
 import numpy as np
 from scipy import stats as spst
 import porespy as ps
 import openpnm as op
-import zipfile
 from edt import edt
-from pathlib import Path
 ws = op.Workspace()
 ws.settings['loglevel'] = 50
 ps.settings.tqdm['disable'] = True
@@ -20,7 +17,7 @@ class Snow2Test:
         self.spheres2D = ~ps.generators.lattice_spheres(shape=[500, 500], r=30,
                                                         spacing=56, offset=25)
 
-    def test_single_phase_2D_serial(self):
+    def test_single_phase_2d_serial(self):
         im = ps.generators.blobs(shape=[200, 200])
         snow2 = ps.networks.snow2(im, phase_alias={1: 'phase1'}, parallelization=None)
         pn, geo = op.io.PoreSpy.import_data(snow2.network)
@@ -34,7 +31,7 @@ class Snow2Test:
         assert hasattr(snow2, 'regions')
         assert hasattr(snow2, 'phases')
 
-    def test_multiphase_2D(self):
+    def test_multiphase_2d(self):
         im1 = ps.generators.blobs(shape=[200, 200], porosity=0.4)
         im2 = ps.generators.blobs(shape=[200, 200], porosity=0.7)
         phases = im1 + (im2 * ~im1)*2
@@ -45,14 +42,14 @@ class Snow2Test:
         assert 'pore.test2' in pn.keys()
         assert 'pore.phase2' not in pn.keys()
 
-    def test_single_phase_3D(self):
+    def test_single_phase_3d(self):
         im = ps.generators.blobs(shape=[100, 100, 100], porosity=0.6)
         snow2 = ps.networks.snow2(im, phase_alias={1: 'phase1'})
         pn, geo = op.io.PoreSpy.import_data(snow2.network)
         # Ensure phase_alias was ignored since only single phase
         assert 'pore.phase1' not in pn.keys()
 
-    def test_multiphase_3D(self):
+    def test_multiphase_3d(self):
         im1 = ps.generators.blobs(shape=[100, 100, 100], porosity=0.4)
         im2 = ps.generators.blobs(shape=[100, 100, 100], porosity=0.7)
         phases = im1 + (im2 * ~im1)*2
@@ -62,49 +59,56 @@ class Snow2Test:
         assert 'pore.phase1' in pn.keys()
         assert 'pore.phase2' in pn.keys()
 
-    def test_parse_pad_width_2D(self):
-        s = [10, 10]
-        pw = ps.networks._parse_pad_width(1, s)
-        assert np.all(pw == [[1, 1], [1, 1]])
-        pw = ps.networks._parse_pad_width(0, s)
-        assert np.all(pw == [[0, 0], [0, 0]])
-        pw = ps.networks._parse_pad_width([1, 2], s)
-        assert np.all(pw == [[1, 2], [1, 2]])
-        pw = ps.networks._parse_pad_width([1, 0], s)
-        assert np.all(pw == [[1, 0], [1, 0]])
-        pw = ps.networks._parse_pad_width([[1, 2], [3, 4]], s)
-        assert np.all(pw == [[1, 2], [3, 4]])
-        pw = ps.networks._parse_pad_width([1, [2, 3]], s)
-        assert np.all(pw == [[1, 1], [2, 3]])
-        pw = ps.networks._parse_pad_width([0, [2, 3]], s)
-        assert np.all(pw == [[0, 0], [2, 3]])
-        pw = ps.networks._parse_pad_width([0, [0, 3]], s)
-        assert np.all(pw == [[0, 0], [0, 3]])
-        pw = ps.networks._parse_pad_width([[1], [2, 3]], s)
-        assert np.all(pw == [[1, 1], [2, 3]])
-        with pytest.raises(Exception):
-            pw = ps.networks._parse_pad_width([[1], [2, 3], 0], s)
-        with pytest.raises(Exception):
-            pw = ps.networks._parse_pad_width([0, 1, 2], s)
+    def test_parse_pad_width_2d(self):
+        shape_2d = [10, 10]
+        test_cases_2d = [
+            [5,                 [[5, 5], [5, 5]]],
+            [0,                 [[0, 0], [0, 0]]],
+            [[1, 0],            [[1, 1], [0, 0]]],
+            [[0, [2, 3]],       [[0, 0], [2, 3]]],
+            [[0, [0, 3]],       [[0, 0], [0, 3]]],
+            [[5],               [[5, 5], [5, 5]]],
+            [[[5]],             [[5, 5], [5, 5]]],
+            [[1, [2, 3]],       [[1, 1], [2, 3]]],
+            [[[1], [2, 3]],     [[1, 1], [2, 3]]],
+            [[1, 5],            [[1, 1], [5, 5]]],
+            [[[[1]], [5]],      [[1, 1], [5, 5]]],
+            [[[1, 2], [3, 4]],  [[1, 2], [3, 4]]],
+            [[1, 2, 3],         "pad_width must be scalar or 2-element list"],
+            [[1, [2, 3], 4],    "pad_width must be scalar or 2-element list"],
+            [[1, [2, 3, 3]],    "pad_width components can't have 2+ elements"]
+        ]
+        for case, out_desired in test_cases_2d:
+            try:
+                out = ps.networks._parse_pad_width(case, shape_2d).tolist()
+            except Exception as e:
+                out = e.args[0]
+            assert out == out_desired
 
-    def test_parse_pad_width_3D(self):
-        s = [10, 10, 10]
-        pw = ps.networks._parse_pad_width(1, s)
-        assert np.all(pw == [[1, 1], [1, 1], [1, 1]])
-        pw = ps.networks._parse_pad_width(0, s)
-        assert np.all(pw == [[0, 0], [0, 0], [0, 0]])
-        pw = ps.networks._parse_pad_width([0, 1], s)
-        assert np.all(pw == [[0, 1], [0, 1], [0, 1]])
-        pw = ps.networks._parse_pad_width([0, [1, 2], 3], s)
-        assert np.all(pw == [[0, 0], [1, 2], [3, 3]])
-        pw = ps.networks._parse_pad_width([0, [1, 2], [3, 4]], s)
-        assert np.all(pw == [[0, 0], [1, 2], [3, 4]])
-        pw = ps.networks._parse_pad_width([[1], [2, 3], 0], s)
-        assert np.all(pw == [[1, 1], [2, 3], [0, 0]])
-        with pytest.raises(Exception):
-            pw = ps.networks._parse_pad_width([0, 1, 2], s)
-        with pytest.raises(Exception):
-            pw = ps.networks._parse_pad_width([], s)
+    def test_parse_pad_width_3d(self):
+        shape_3d = [10, 10, 10]
+        test_cases_3d = [
+            [5,                         [[5, 5], [5, 5], [5, 5]]],
+            [0,                         [[0, 0], [0, 0], [0, 0]]],
+            [[5],                       [[5, 5], [5, 5], [5, 5]]],
+            [[[5]],                     [[5, 5], [5, 5], [5, 5]]],
+            [[1, 0, 0],                 [[1, 1], [0, 0], [0, 0]]],
+            [[1, [2, 3]],               "pad_width must be scalar or 3-element list"],
+            [[1, 5],                    "pad_width must be scalar or 3-element list"],
+            [[[[1]], [5]],              "pad_width must be scalar or 3-element list"],
+            [[[1, 2, 1], 3, 4],         "pad_width components can't have 2+ elements"],
+            [[0, [1, 2], [3, 4]],       [[0, 0], [1, 2], [3, 4]]],
+            [[1, 2, 3],                 [[1, 1], [2, 2], [3, 3]]],
+            [[1, [2, 3], 4],            [[1, 1], [2, 3], [4, 4]]],
+            [[[1, 2], [3, 4], [5, 6]],  [[1, 2], [3, 4], [5, 6]]],
+            [[[1, 2], 5, [5, 6]],       [[1, 2], [5, 5], [5, 6]]]
+        ]
+        for case, out_desired in test_cases_3d:
+            try:
+                out = ps.networks._parse_pad_width(case, shape_3d).tolist()
+            except Exception as e:
+                out = e.args[0]
+            assert out == out_desired
 
     def test_label_phases(self):
         im = self.spheres2D
@@ -120,7 +124,7 @@ class Snow2Test:
         assert 'pore.void' in snow.network.keys()
         assert 'pore.solid' in snow.network.keys()
 
-    def test_ensure_correct_sizes_are_returned_single_phase_2D(self):
+    def test_ensure_correct_sizes_are_returned_single_phase_2d(self):
         im = self.spheres2D
         snow = ps.networks.snow2(phases=im, parallelization=None)
         mode = spst.mode(snow.network['pore.extended_diameter'])
@@ -128,7 +132,7 @@ class Snow2Test:
         D = np.unique(snow.network['pore.extended_diameter'].astype(int))
         assert np.all(D == np.array([30, 34, 60]))
 
-    def test_ensure_correct_sizes_are_returned_dual_phase_2D(self):
+    def test_ensure_correct_sizes_are_returned_dual_phase_2d(self):
         im = self.spheres2D
         phases = im.astype(int) + 1
         snow = ps.networks.snow2(phases=phases, parallelization=None)
@@ -138,7 +142,7 @@ class Snow2Test:
         assert np.all(D == np.array([15, 16, 17, 18, 19, 21,
                                       22, 25, 30, 32, 34, 60]))
 
-    def test_ensure_correct_sizes_are_returned_single_phase_3D(self):
+    def test_ensure_correct_sizes_are_returned_single_phase_3d(self):
         im = self.spheres3D
         snow = ps.networks.snow2(phases=im, parallelization=None)
         mode = spst.mode(snow.network['pore.extended_diameter'])
@@ -146,7 +150,7 @@ class Snow2Test:
         D = np.unique(snow.network['pore.extended_diameter'].astype(int))
         assert np.all(D == np.array([30, 33, 60]))
 
-    def test_ensure_correct_sizes_are_returned_dual_phase_3D(self):
+    def test_ensure_correct_sizes_are_returned_dual_phase_3d(self):
         im = self.spheres3D
         phases = im.astype(int) + 1
         snow = ps.networks.snow2(phases=phases, parallelization=None)
@@ -171,16 +175,16 @@ class Snow2Test:
         im = ps.generators.blobs([200, 200, 200], porosity=0.6, blobiness=1.5)
 
         snow_1 = ps.networks.snow2(im,
-                                   accuracy='standard',
-                                   parallelization=None)
+                                    accuracy='standard',
+                                    parallelization=None)
         pn1, geo1 = op.io.PoreSpy.import_data(snow_1.network)
         Ps1 = pn1.find_neighbor_pores(pores=pn1.pores('boundary'))
         Ps1 = pn1.tomask(pores=Ps1)
 
         snow_2 = ps.networks.snow2(im.astype(int) + 1,
-                                   phase_alias={1: 'solid', 2: 'void'},
-                                   accuracy='standard',
-                                   parallelization=None)
+                                    phase_alias={1: 'solid', 2: 'void'},
+                                    accuracy='standard',
+                                    parallelization=None)
         pn2, geo2 = op.io.PoreSpy.import_data(snow_2.network)
         Ps2 = pn2.find_neighbor_pores(pores=pn2.pores('boundary'))
         Ps2 = pn2.tomask(pores=Ps2)*pn2['pore.void']
@@ -190,8 +194,8 @@ class Snow2Test:
         assert pn1.num_throats('all') == pn2.num_throats('void_void')
 
         snow_3 = ps.networks.snow2(im==0,
-                                   accuracy='standard',
-                                   parallelization=None)
+                                    accuracy='standard',
+                                    parallelization=None)
         pn3, geo3 = op.io.PoreSpy.import_data(snow_3.network)
         Ps3 = pn3.find_neighbor_pores(pores=pn3.pores('boundary'))
         Ps3 = pn3.tomask(pores=Ps3)
