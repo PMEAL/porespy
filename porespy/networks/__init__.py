b9c5f9513ebdbbaab53923f5b66a2b8289511938
jgostick <jgostick@gmail.com>
2021-07-19 17:50:08 -0400

    Added the word wrapper to maximal_ball func

--- porespy/networks/__init__.py
+++ porespy/networks/__init__.py
@@ -13,32 +13,24 @@ Contains functions for analysing images as pore networks.
    :template: mybase.rst
    :toctree: generated/
 
-   networks.snow2
-   networks.map_to_regions
-   networks.regions_to_network
    networks.add_boundary_regions
    networks.generate_voxel_image
+   networks.label_phases
+   network.label_boundaries
+   networks.map_to_regions
    networks.maximal_ball
+   networks.regions_to_network
+   networks.snow2
 
 """
 
-__all__ = [
-    "add_boundary_regions",
-    "map_to_regions",
-    "generate_voxel_image",
-    "label_boundary_cells",
-    "add_phase_interconnections",
-    "regions_to_network",
-    "snow2",
-    "maximal_ball"]
-
 from ._funcs import add_boundary_regions
-from ._funcs import map_to_regions
 from ._funcs import generate_voxel_image
 from ._funcs import label_phases
 from ._funcs import label_boundaries
+from ._funcs import map_to_regions
+from ._maximal_ball import maximal_ball_wrapper
 from ._getnet import regions_to_network
-from ._utils import _net_dict
 from ._snow2 import snow2
+from ._utils import _net_dict
 from ._snow2 import _parse_pad_width
-from ._maximal_ball import maximal_ball
