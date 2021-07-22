bc6ccc29defb88887dd5b61c8c914b219c53e538
Amin Sadeghi <amin.sadeghi@live.com>
2021-07-22 13:01:06 -0400

    Exclude pyimagej from coverage, it's tricky to get CI to work with it
    we'll fix it later

--- test/unit/test_imagej.py
+++ test/unit/test_imagej.py
@@ -2,12 +2,13 @@ import os
 import sys
 import porespy as ps
 
+
 class ImageJTest:
 
     def setup_class(self):
         self.path = os.path.dirname(os.path.abspath(sys.argv[0]))
 
-    def test_imagej_wrapper(self): 
+    def test_imagej_wrapper(self):
         img = ps.generators.blobs(shape=[50, 50, 50], porosity=.5, blobiness=2)
         plgn = ps.filters.imagej.imagej_wrapper(img, 'mean', 'sc.fiji:fiji:2.1.1')
         assert sum(plgn.shape) == 150
@@ -15,8 +16,8 @@ class ImageJTest:
     def test_imagej_plugin(self):
         img = ps.generators.blobs(shape=[50, 50, 50], porosity=.5, blobiness=2)
         test = ps.filters.imagej.imagej_plugin(img, path='sc.fiji:fiji:2.1.1',
-                                        plugin_name='Mean 3D...',
-                                        args={'options' : 'True'})
+                                               plugin_name='Mean 3D...',
+                                               args={'options' : 'True'})
         assert sum(test.shape) == 150
 
 
