a7ab9d31facf7d987bf048cb152a45162b4a5626
Jeff Gostick <jgostick@gmail.com>
2022-01-24 23:40:21 -0500

    accepting inf in drainage tests

--- test/unit/test_simulations.py
+++ test/unit/test_simulations.py
@@ -18,15 +18,20 @@ class SimulationsTest():
         self.im_dt = edt(self.im)
 
     def test_drainage_with_gravity(self):
-        np.random.seed(0)
-        im = ps.generators.blobs(shape=[100, 100])
+        np.random.seed(2)
+        im = ps.generators.blobs(shape=[100, 100], porosity=0.7)
         dt = edt(im)
         pc = -2*0.072*np.cos(np.deg2rad(180))/dt
         np.testing.assert_approx_equal(pc[im].max(), 0.144)
         drn = ps.simulations.drainage(pc=pc, im=im, voxel_size=1e-5, g=9.81)
-        np.testing.assert_approx_equal(drn.im_pc.max(), 9.855899)
+        np.testing.assert_approx_equal(drn.im_pc.max(), np.inf)
         drn2 = ps.simulations.drainage(pc=pc, im=im, voxel_size=1e-4, g=0)
-        np.testing.assert_approx_equal(drn2.im_pc[im].max(), 0.1036005)
+        np.testing.assert_approx_equal(drn2.im_pc[im].max(), np.inf)
+        im = ps.filters.fill_blind_pores(im)
+        drn = ps.simulations.drainage(pc=pc, im=im, voxel_size=1e-5, g=9.81)
+        np.testing.assert_approx_equal(drn.im_pc.max(), 10.04657972914)
+        drn2 = ps.simulations.drainage(pc=pc, im=im, voxel_size=1e-4, g=0)
+        np.testing.assert_approx_equal(drn2.im_pc[im].max(), 0.14622522289864)
 
 
 if __name__ == '__main__':
