4bccc67cfa01384b1239550876069bb984abdb4e
Jeff Gostick <jgostick@gmail.com>
2022-01-24 22:48:36 -0500

    adding integration tests to resproduce (approximately) the WRR paper

--- test/integration/test_drainage_from_top.py
+++ test/integration/test_drainage_from_top.py
@@ -25,39 +25,43 @@ bg = 'grey'
 drn1 = ps.simulations.drainage(im=im,
                                voxel_size=voxel_size,
                                inlets=inlets,
+                               delta_rho=delta_rho,
                                g=g)
 drn2 = ps.simulations.drainage(im=im,
                                voxel_size=voxel_size,
                                inlets=inlets,
                                outlets=outlets,
+                               delta_rho=delta_rho,
                                g=g)
 drn3 = ps.simulations.drainage(im=im,
                                voxel_size=voxel_size,
                                inlets=inlets,
                                residual=residual,
+                               delta_rho=delta_rho,
                                g=g)
 drn4 = ps.simulations.drainage(im=im,
                                voxel_size=voxel_size,
                                inlets=inlets,
                                outlets=outlets,
                                residual=residual,
+                               delta_rho=delta_rho,
                                g=g)
 
 # Ensure initial saturations correspond to amount of residual present
 assert drn1.snwp[0] == 0
 assert drn2.snwp[0] == 0
-assert drn3.snwp[0] == 0.34427115020497745
-assert drn4.snwp[0] == 0.34427115020497745
+assert drn3.snwp[0] == 0.5236847043036736
+assert drn4.snwp[0] == 0.44698731471004644
 
 # Ensure final saturations correspond to trapping
 assert drn1.snwp[-1] == 1
-assert drn2.snwp[-1] == 0.7989685739141312
+assert drn2.snwp[-1] == 0.925673639501231
 assert drn3.snwp[-1] == 1
-assert drn4.snwp[-1] == 0.6365388661136161
+assert drn4.snwp[-1] == 0.7995542237864823
 
 
 # %% Visualize the invasion configurations for each scenario
-if 1:
+if 0:
     fig, ax = plt.subplots(2, 2, facecolor=bg)
     ax[0][0].imshow(drn1.im_satn/im, origin='lower')
     ax[0][0].set_title("No trapping, no residual")
@@ -69,8 +73,22 @@ if 1:
     ax[1][1].set_title("With trapping, with residual")
 
 
+# %% Visualize the capillary pressure map for each scenario
+if 0:
+    fig, ax = plt.subplots(2, 2, facecolor=bg)
+    ax[0][0].imshow(drn1.im_pc/im, origin='lower')
+    ax[0][0].set_title("No trapping, no residual")
+    ax[0][1].imshow(drn2.im_pc/im, origin='lower')
+    ax[0][1].set_title("With trapping, no residual")
+    ax[1][0].imshow(drn3.im_pc/im, origin='lower')
+    ax[1][0].set_title("No trapping, with residual")
+    ax[1][1].imshow(drn4.im_pc/im, origin='lower')
+    ax[1][1].set_title("With trapping, with residual")
+
+
+
 # %% Plot the capillary pressure curves for each scenario
-if 1:
+if 0:
     plt.figure(facecolor=bg)
     ax = plt.axes()
     ax.set_facecolor(bg)
