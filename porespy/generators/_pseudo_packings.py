ff4ec9dac0bb61a9770c6958f515bf1d5f3d21da
Jeff Gostick <jgostick@gmail.com>
2022-03-08 22:14:27 -0500

    refactored gravity packing to use pseudo_electrostatic packing

--- porespy/generators/_pseudo_packings.py
+++ porespy/generators/_pseudo_packings.py
@@ -4,14 +4,16 @@ import scipy.ndimage as spim
 from edt import edt
 from skimage.morphology import disk, ball
 from porespy import settings
-from porespy.tools import get_tqdm, ps_round
+from porespy.tools import get_tqdm, ps_round, get_border
 from porespy.tools import insert_disks_at_points
 from porespy.filters import trim_disconnected_blobs, fftmorphology
 from loguru import logger
+import random
 tqdm = get_tqdm()
 
 
-def pseudo_gravity_packing(im, r, clearance=0, axis=0, maxiter=1000):
+def pseudo_gravity_packing(im, r, clearance=0, axis=0, maxiter=1000,
+                           edges='contained'):
     r"""
     Iteratively inserts spheres at the lowest accessible point in an image,
     mimicking a gravity packing.
@@ -31,6 +33,16 @@ def pseudo_gravity_packing(im, r, clearance=0, axis=0, maxiter=1000):
         The axis along which gravity acts.
     maxiter : int (default is 1000)
         The maximum number of spheres to add
+    edges : string (default is 'contained')
+        Controls how the edges of the image are handled.  Options are:
+
+        'contained'
+            Spheres are all completely within the image
+        'extended'
+            Spheres are allowed to extend beyond the edge of the
+            image.  In this mode the volume fraction will be less that
+            requested since some spheres extend beyond the image, but their
+            entire volume is counted as added for computational efficiency.
 
     Returns
     -------
@@ -41,48 +53,21 @@ def pseudo_gravity_packing(im, r, clearance=0, axis=0, maxiter=1000):
 
     """
     logger.debug(f'Adding spheres of radius {r}')
-    im = np.swapaxes(im, 0, axis)
-    im_temp = np.zeros_like(im, dtype=bool)
-    r = r - 1
-    strel = disk if im.ndim == 2 else ball
-    sites = fftmorphology(im == 1, strel=strel(r), mode='erosion')
-    inlets = np.zeros_like(im)
-    inlets[-(r+1), ...] = True
-    sites = trim_disconnected_blobs(im=sites, inlets=inlets)
-    x_min = np.where(sites)[0].min()
-    n = None
-    for n in tqdm(range(maxiter), **settings.tqdm):
-        if im.ndim == 2:
-            x, y = np.where(sites[x_min:x_min+2*r, ...])
-        else:
-            x, y, z = np.where(sites[x_min:x_min+2*r, ...])
-        if len(x) == 0:
-            break
-        options = np.where(x == x.min())[0]
-        choice = np.random.randint(len(options))
-        if im.ndim == 2:
-            cen = np.vstack([x[options[choice]] + x_min,
-                             y[options[choice]]])
-        else:
-            cen = np.vstack([x[options[choice]] + x_min,
-                             y[options[choice]],
-                             z[options[choice]]])
-        im_temp = insert_disks_at_points(im_temp, coords=cen,
-                                         radii=np.array([r - clearance]),
-                                         v=True, overwrite=True)
-        sites = insert_disks_at_points(sites, coords=cen,
-                                       radii=np.array([2*r]),
-                                       v=0,
-                                       overwrite=True)
-        x_min += x.min()
-    logger.debug(f'A total of {n} spheres were added')
-    im_temp = np.swapaxes(im_temp, 0, axis)
+    sites = np.zeros_like(im)
+    sites = np.swapaxes(sites, 0, axis)
+    sites[0, ...] = True
+    sites = np.swapaxes(sites, 0, axis)
+    im_temp = pseudo_electrostatic_packing(im=im,
+                                           r=r, sites=sites,
+                                           clearance=clearance,
+                                           edges=edges)
     return im_temp
 
 
 def pseudo_electrostatic_packing(im, r, sites=None,
                                  clearance=0,
                                  protrusion=0,
+                                 edges='extended',
                                  maxiter=1000):
     r"""
     Iterativley inserts spheres as close to the given sites as possible.
@@ -104,6 +89,16 @@ def pseudo_electrostatic_packing(im, r, sites=None,
         The amount that spheres are allowed to protrude beyond the active phase.
     maxiter : int (optional, default=1000)
         The maximum number of spheres to insert.
+    edges : string (default is 'contained')
+        Controls how the edges of the image are handled.  Options are:
+
+        'contained'
+            Spheres are all completely within the image
+        'extended'
+            Spheres are allowed to extend beyond the edge of the
+            image.  In this mode the volume fraction will be less that
+            requested since some spheres extend beyond the image, but their
+            entire volume is counted as added for computational efficiency.
 
     Returns
     -------
@@ -124,6 +119,9 @@ def pseudo_electrostatic_packing(im, r, sites=None,
     else:
         dtmax = min(im.shape)
     dt[~sites] = dtmax
+    if edges == 'contained':
+        borders = get_border(im.shape, thickness=r, mode='faces')
+        dt[borders] = dtmax
     r = r + clearance
     # Get initial options
     options = np.where(dt == 1)
@@ -136,7 +134,7 @@ def pseudo_electrostatic_packing(im, r, sites=None,
             hits = dt[options] < dtmax
         if hits.size == 0:
             break
-        choice = np.where(hits)[0][0]
+        choice = random.choice(np.where(hits)[0])
         cen = np.vstack([options[i][choice] for i in range(im.ndim)])
         im_temp = insert_disks_at_points(im_temp, coords=cen,
                                          radii=np.array([r-clearance]),
@@ -152,6 +150,19 @@ def pseudo_electrostatic_packing(im, r, sites=None,
 if __name__ == "__main__":
     import porespy as ps
     import matplotlib.pyplot as plt
-    im = ps.generators.pseudo_gravity_packing(im=np.ones([200, 200]), r=10)
-    plt.imshow(im)
+    shape = [200, 200]
+
+    fig, ax = plt.subplots(1, 2)
+    im = ps.generators.pseudo_gravity_packing(im=np.ones(shape, dtype=bool),
+                                              r=7, clearance=3,
+                                              edges='contained')
+    ax[0].imshow(im, origin='lower')
+
+    sites = np.zeros(shape, dtype=bool)
+    sites[100, 100] = True
+    im = ps.generators.pseudo_electrostatic_packing(im=np.ones(shape, dtype=bool),
+                                                    r=5, sites=sites,
+                                                    clearance=4,
+                                                    maxiter=50)
+    ax[1].imshow(im, origin='lower')
 
