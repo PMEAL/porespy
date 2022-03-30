d4a377a2cdde5f28f8a066c8ecc578e9ecfe9832
jgostick <jgostick@uwaterloo.ca>
2022-03-30 15:08:00 -0400

    adding urls to metrics

--- porespy/metrics/_meshtools.py
+++ porespy/metrics/_meshtools.py
@@ -37,6 +37,12 @@ def region_volumes(regions, mode='marching_cubes'):
         An array of shape [N by 1] where N is the number of labelled regions
         in the image.
 
+    Examples
+    --------
+    `Click here
+    <https://porespy.org/examples/metrics/reference/mesh_volumes.html>`_
+    to view online example.
+
     """
     slices = spim.find_objects(regions)
     vols = np.zeros([len(slices), ])
@@ -65,6 +71,12 @@ def mesh_volume(region):
         The volume of the region computed by applyuing the marching cubes
         algorithm to the region, then finding the mesh volume using the
         ``trimesh`` package.
+
+    Examples
+    --------
+    `Click here
+    <https://porespy.org/examples/metrics/reference/mesh_volume.html>`_
+    to view online example.
 
     """
     mc = mesh_region(region > 0)
@@ -104,6 +116,12 @@ def region_surface_areas(regions, voxel_size=1, strel=None):
     areas : list
         A list containing the surface area of each region, offset by 1, such
         that the surface area of region 1 is stored in element 0 of the list.
+
+    Examples
+    --------
+    `Click here
+    <https://porespy.org/examples/metrics/reference/region_surface_areas.html>`_
+    to view online example.
 
     """
     logger.trace('Finding surface area of each region')
@@ -155,6 +173,12 @@ def mesh_surface_area(mesh=None, verts=None, faces=None):
     it allows for the passing of the ``mesh`` tuple returned by the
     ``mesh_region`` function, entirely for convenience.
 
+    Examples
+    --------
+    `Click here
+    <https://porespy.org/examples/metrics/reference/mesh_surface_area.html>`_
+    to view online example.
+
     """
     if mesh:
         verts = mesh.verts
@@ -204,6 +228,12 @@ def region_interface_areas(regions, areas, voxel_size=1, strel=None):
 
         'area'
             The area calculated for each pair of regions in ``conns``
+
+    Examples
+    --------
+    `Click here
+    <https://porespy.org/examples/metrics/reference/region_interface_areas.html>`_
+    to view online example.
 
     """
     logger.trace('Finding interfacial areas between each region')
