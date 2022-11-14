7b84084feea5aba8412aff79f8197b8460902e83
jgostick <jgostick@uwaterloo.ca>
2022-11-14 15:26:14 -0500

    using numpy method

--- porespy/generators/_spheres_from_coords.py
+++ porespy/generators/_spheres_from_coords.py
@@ -1,5 +1,6 @@
 import numpy as np
 from porespy.tools import insert_sphere
+from porespy.tools import _insert_disks_at_points
 
 
 __all__ = [
@@ -7,7 +8,7 @@ __all__ = [
 ]
 
 
-def spheres_from_coords(df, voxel_size, shape=None):
+def spheres_from_coords(df, voxel_size, maxdim=500):
     r"""
     Generates a sphere packing given a list of centers and radii
 
@@ -16,10 +17,17 @@ def spheres_from_coords(df, voxel_size, shape=None):
     df : DataFrame or dict
         The X, Y, Z center coordinates and R of each sphere in the packing
         should be stored in their own column or key. See ``Notes`` for more
-        detail.
+        detail on how this should be formatted. If one of the dimensions has
+        all 0's then a 2D image is generated.
     voxel_size : float
         The resolution of the image to generate in terms of how many 'units'
         in the supplied packing data correspond to the side-length of a voxel.
+    maxdim : int
+        The maximum allowable size for the largest dimension of the image.
+        This is set to 500 to ensure that the largest possible image is
+        $500^3$, which is managable on most computers. If one of the dimensions
+        is larger than this an ``Exception`` is raised.  The can happen if the
+        ``voxel_size`` is very small for instance.
 
     Returns
     -------
@@ -29,7 +37,7 @@ def spheres_from_coords(df, voxel_size, shape=None):
 
     Notes
     -----
-    The input data should be in column format, either as a dictionary of 1D
+    The input data should be in column formatm as a dictionary of 1D
     *numpy* arrays like this:
 
     .. code::
@@ -57,7 +65,7 @@ def spheres_from_coords(df, voxel_size, shape=None):
                [1, 3, 1, 0.7],
                [2, 2, 1, 0.6]])
 
-    Note that in all cases if the radius is not given that it is assume to be
+    Note that in all cases if the radius is not given that it is assumed to be
     a single pixel
 
     """
@@ -78,14 +86,15 @@ def spheres_from_coords(df, voxel_size, shape=None):
     y = ((df['Y'] - df['Y'].min() + df['R'].max())/vx).astype(int)
     z = ((df['Z'] - df['Z'].min() + df['R'].max())/vx).astype(int)
     try:  # Add a try/except to catch files with no radius to use r=1 voxel
-        r = (df['R']/vx).astype(int)
+        r = np.array(df['R']/vx, dtype=int)
     except KeyError:
         r = np.ones_like(x)
     R = r.max()
     shape = np.ceil([x.max()+R+1, y.max()+R+1, z.max()+R+1]).astype(int)
+    if max(shape) >= maxdim:
+        raise Exception(f'The expected image size is too large: {shape}')
     im = np.zeros(shape, dtype=bool)
-    for i in range(len(x)):
-        im = insert_sphere(im, c=[x[i], y[i], z[i]], r=r[i], v=True)
+    im = _insert_disks_at_points(im, coords=np.vstack([x, y, z]), radii=r, v=True, smooth=False)
     return im
 
 
@@ -96,8 +105,17 @@ if __name__ == '__main__':
     import imageio
     import porespy as ps
 
-    df = pd.read_csv('point map.csv')
-    df['R'] = 1.5
+    f = r'C:\Users\jeff\OneDrive - University of Waterloo\Research\NRC - Electrolyzers\point map.csv'
+    df = pd.read_csv(f)
+    df['R'] = df['pore size']/2
     im = spheres_from_coords(df, voxel_size=.1)
     # plt.imshow(ps.visualization.sem(~im, axis=0))
     # imageio.volsave('spheres.tif', np.array(im, dtype=int))
+
+    from vedo import Volume, show
+    temp = 4.0*im
+    vol = Volume(temp)
+    lego = vol.legosurface(vmin=1, vmax=temp.max(), boundary=False)
+    # lego.cmap('jet', on='cells', vmin=1, vmax=temp.max())
+    lego.add_scalarbar()
+    show(lego).close()
