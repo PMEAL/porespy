f019027a0d1b24cd169a5e96906d05effe12beaf
Jeff Gostick <jgostick@gmail.com>
2024-02-03 18:02:07 -0500

    adding cylindrical_pillars

--- porespy/generators/_micromodels.py
+++ porespy/generators/_micromodels.py
@@ -1,7 +1,8 @@
-import porespy as ps
 import numpy as np
 import matplotlib.pyplot as plt
-from porespy import beta
+from porespy.generators import lattice_spheres
+from porespy.tools import _insert_disks_at_points_parallel
+from porespy.tools import extend_slice, extract_subsection
 import scipy.ndimage as spim
 from typing import List
 
@@ -23,37 +24,43 @@ def rectangular_pillars(
     shape = np.array(shape)
     new_shape = (np.ones_like(shape)*shape.max()*2).astype(int)
     if lattice.startswith('s'):
-        pts = ~ps.generators.lattice_spheres(new_shape, r=1, spacing=spacing, offset=0)
+        pts = ~lattice_spheres(new_shape, r=1, spacing=spacing, offset=0)
     elif lattice.startswith('t'):
-        pts = ~ps.generators.lattice_spheres(shape=new_shape, r=1, spacing=spacing, offset=0)
+        pts = ~lattice_spheres(shape=new_shape, r=1, spacing=spacing, offset=0)
     if Rmax is None:
         Rmax = int(spacing/2)
     labels = spim.label(pts)[0]
     tmp = np.zeros_like(pts)
     slices = spim.find_objects(labels)
     for s in slices:
-        sx = ps.tools.extend_slice(
+        sx = extend_slice(
             slices=s,
             shape=pts.shape,
             pad=[np.random.randint(Rmin, Rmax), spacing],
         )
         tmp[sx] = True
-        sx = ps.tools.extend_slice(
+        sx = extend_slice(
             slices=s,
             shape=pts.shape,
             pad=[spacing, np.random.randint(Rmin, Rmax)],
         )
         tmp[sx] = True
+    tmp, pts = _extract([tmp, pts], shape, spacing, truncate, lattice)
+    return tmp, pts
+
+
+def _extract(ims, shape, spacing, truncate, lattice):
     if lattice.startswith('s'):
         if truncate:
             end = shape
         else:
             end = (np.ceil(shape/spacing)*spacing).astype(int) + 1
-        tmp = tmp[:end[0], :end[1]]
-        pts = pts[:end[0], :end[1]]
+        for i in range(len(ims)):
+            ims[i] = ims[i][:end[0], :end[1]]
     if lattice.startswith('t'):
-        tmp = spim.rotate(tmp, -45, order=0, reshape=False)
-        pts = spim.rotate(pts*1.0, -45, order=5, reshape=False) > 0.4
+        new_shape = np.array(ims[0].shape)
+        for i in range(len(ims)):
+            ims[i] = spim.rotate(ims[i], -45, order=0, reshape=False)
         a, b = (new_shape/2).astype(int)
         s = (slice(a, a+1, None), slice(b, b+1, None))
         if truncate:
@@ -61,29 +68,64 @@ def rectangular_pillars(
         else:
             diag = np.around((spacing**2 + spacing**2)**0.5).astype(int)
             a, b = (np.ceil(shape/diag)*diag/2).astype(int)
-        sx = ps.tools.extend_slice(slices=s, shape=pts.shape, pad=[a, b])
-        tmp = tmp[sx]
-        pts = pts[sx]
+        sx = extend_slice(slices=s, shape=ims[0].shape, pad=[a, b])
+        for i in range(len(ims)):
+            ims[i] = ims[i][sx]
+    return ims
+
+
+def cylindrical_pillars(shape, spacing, Rmin=5, Rmax=None, lattice='sc', truncate=True):
+    if Rmax is None:
+        Rmax = int(spacing/2)
+    shape = np.array(shape)
+    new_shape = (np.ones_like(shape)*shape.max()*2).astype(int)
+    if lattice.startswith('s'):
+        pts = ~lattice_spheres(new_shape, r=1, spacing=spacing, offset=0)
+    elif lattice.startswith('t'):
+        pts = ~lattice_spheres(new_shape, r=1, spacing=spacing, offset=0)
+    coords = np.vstack(np.where(pts))
+    radii = np.random.randint(Rmin, Rmax, pts.sum())
+    tmp = np.zeros_like(pts, dtype=int)
+    tmp = _insert_disks_at_points_parallel(
+        im=tmp, coords=coords, radii=radii, v=1, smooth=True)
+    if lattice.startswith('s'):
+        if truncate:
+            end = shape
+        else:
+            end = (np.ceil(shape/spacing)*spacing).astype(int) + 1
+        tmp = tmp[:end[0], :end[1]]
+        pts = pts[:end[0], :end[1]]
+    tmp, pts = _extract([tmp, pts], shape, spacing, truncate, lattice)
     return tmp, pts
 
 
 if __name__ == "__main__":
+    rect_demo = False
+    cyl_demo = True
+    if rect_demo:
+        fig, ax = plt.subplots(2, 2)
+        np.random.seed(0)
+        im1, pts1 = rectangular_pillars(
+            shape=[400, 600], Rmin=2, Rmax=15, spacing=40, lattice='simple', truncate=True)
+        im2, pts2 = rectangular_pillars(
+            shape=[400, 600], Rmin=2, Rmax=15, spacing=40, lattice='tri', truncate=True)
 
-    fig, ax = plt.subplots(2, 2)
-    np.random.seed(0)
-    im1, pts1 = rectangular_pillars(
-        shape=[400, 600], Rmin=2, Rmax=15, spacing=40, lattice='simple', truncate=True)
-    im2, pts2 = rectangular_pillars(
-        shape=[400, 600], Rmin=2, Rmax=15, spacing=40, lattice='tri', truncate=True)
+        ax[0][0].imshow(im1 + 2.0*pts1, origin='lower', interpolation='none')
+        ax[0][1].imshow(im2 + 2.0*pts2, origin='lower', interpolation='none')
 
-    ax[0][0].imshow(im1 + 2.0*pts1, origin='lower', interpolation='none')
-    ax[0][1].imshow(im2 + 2.0*pts2, origin='lower', interpolation='none')
+        np.random.seed(0)
+        im1, pts1 = rectangular_pillars(
+            shape=[400, 600], Rmin=2, Rmax=15, spacing=40, lattice='simple', truncate=False)
+        im2, pts2 = rectangular_pillars(
+            shape=[400, 600], Rmin=2, Rmax=15, spacing=40, lattice='tri', truncate=False)
 
-    np.random.seed(0)
-    im1, pts1 = rectangular_pillars(
-        shape=[400, 600], Rmin=2, Rmax=15, spacing=40, lattice='simple', truncate=False)
-    im2, pts2 = rectangular_pillars(
-        shape=[400, 600], Rmin=2, Rmax=15, spacing=40, lattice='tri', truncate=False)
-
-    ax[1][0].imshow(im1 + 2.0*pts1, origin='lower', interpolation='none')
-    ax[1][1].imshow(im2 + 2.0*pts2, origin='lower', interpolation='none')
+        ax[1][0].imshow(im1 + 2.0*pts1, origin='lower', interpolation='none')
+        ax[1][1].imshow(im2 + 2.0*pts2, origin='lower', interpolation='none')
+    if cyl_demo:
+        fig, ax = plt.subplots()
+        np.random.seed(0)
+        im1, pts1 = cylindrical_pillars(
+            shape=[400, 600], Rmin=5, Rmax=15, spacing=40, lattice='simple', truncate=False)
+        im2, pts2 = cylindrical_pillars(
+            shape=[400, 600], Rmin=5, Rmax=15, spacing=40, lattice='tri')
+        ax.imshow(im1 + 2.0*pts1, origin='lower', interpolation='none')
