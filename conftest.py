9622a8c2b962e364abced9d104c7467485325b8c
Amin Sadeghi <amin.sadeghi@live.com>
2024-03-10 19:20:41 -0400

    Exclude conf.py from tests (pycodestyle complains)

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 # Ignore the following during testing
 collect_ignore = [
+    "docs/conf.py",
     "examples/generators/reference/fractal_noise.ipynb",
     "examples/networks/reference/diffusive_size_factor_AI.ipynb",
     "examples/networks/tutorials/predicting_diffusive_size_factors_rock_sample.ipynb",
