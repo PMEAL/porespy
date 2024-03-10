a6ca4999cb12eabd4effbb6e499898b60d571c75
Amin Sadeghi <amin.sadeghi@live.com>
2024-03-10 19:09:48 -0400

    Add conftest.py for better control over what pytest should ignore

---
+++ conftest.py
@@ -0,0 +1,7 @@
+# Ignore the following during testing
+collect_ignore = [
+    "examples/generators/reference/fractal_noise.ipynb",
+    "examples/networks/reference/diffusive_size_factor_AI.ipynb",
+    "examples/networks/tutorials/predicting_diffusive_size_factors_rock_sample.ipynb",
+    "examples/networks/tutorials/using_diffusive_size_factor_AI_with_snow.ipynb",
+]
