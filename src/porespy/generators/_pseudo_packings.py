3a552c855b149410412152dbadb231352b628680
Jeff Gostick <jgostick@gmail.com>
2024-03-10 14:56:04 -0400

    Fixed gravity packing.
    clearance and edges args now work fine.

--- src/porespy/generators/_pseudo_packings.py
+++ src/porespy/generators/_pseudo_packings.py
@@ -4,11 +4,12 @@ import scipy.ndimage as spim
 from edt import edt
 from skimage.morphology import disk, ball
 from porespy import settings
-from porespy.tools import get_tqdm, ps_round, get_border
+from porespy.tools import get_tqdm, ps_round, get_border, unpad
 from porespy.tools import _insert_disks_at_points
 from porespy.filters import trim_disconnected_blobs, fftmorphology
 from numba import njit
-from typing import Literal
+from typing import Literal, List
+import numpy.typing as npt
 
 
 __all__ = [
@@ -27,14 +28,16 @@ def _set_seed(a):
 
 
 def pseudo_gravity_packing(
-    im,
+    im: npt.ArrayLike = None,
+    shape: List = None,
     r: int = 5,
     clearance: int = 0,
     axis: int = 0,
     edges: Literal['contained', 'extended'] = 'contained',
     maxiter: int = 1000,
     seed: float = None,
-):
+    smooth: bool = True,
+) -> np.ndarray:
     r"""
     Iteratively inserts spheres at the lowest accessible point in an image,
     mimicking a gravity packing.
@@ -51,7 +54,8 @@ def pseudo_gravity_packing(
         The amount space to add between neighboring spheres. The value can be
         negative for overlapping spheres, but ``abs(clearance) > r``.
     axis : int (default is 0)
-        The axis along which gravity acts.
+        The axis along which gravity acts, directed from the end (-1) towards the
+        start (0).
     maxiter : int (default is 1000)
         The maximum number of spheres to add
     edges : string (default is 'contained')
@@ -77,7 +81,7 @@ def pseudo_gravity_packing(
     Returns
     -------
     spheres : ndarray
-        An image the same size as ``im`` with spheres indicated by ``True``.
+        An image the same size as ``im`` with spheres indicated by ``False``.
         The spheres are only inserted at locations that are accessible
         from the top of the image.
 
@@ -95,24 +99,35 @@ def pseudo_gravity_packing(
         np.random.seed(seed)
 
     im = np.swapaxes(im, 0, axis)
-    im_temp = np.zeros_like(im, dtype=bool)
-    r = r - 1
-    strel = disk if im.ndim == 2 else ball
-    sites = fftmorphology(im == 1, strel=strel(r), mode='erosion')
+    im_temp = np.copy(im)
+    if smooth:
+        r = r + 1
+    sites = im == 1
+    if edges == 'contained':
+        pw = ((1, 0), (1, 1), (1, 1)) if im.ndim == 3 else ((1, 0), (1, 1))
+        im_padded = np.pad(im, pad_width=pw, mode='constant', constant_values=False)
+        sites = unpad(edt(im_padded) > r, pw)
+    else:
+        sites = edt(im) > r
     inlets = np.zeros_like(im)
     inlets[-(r+1), ...] = True
-    sites = trim_disconnected_blobs(im=sites, inlets=inlets)
+    s = ball(1) if im.ndim == 3 else disk(1)
+    sites = trim_disconnected_blobs(im=sites, inlets=inlets, strel=s)
     x_min = np.where(sites)[0].min()
     n = None
     for n in tqdm(range(maxiter), **settings.tqdm):
+        # Find all locations in sites within 2R of the current x_min
         if im.ndim == 2:
             x, y = np.where(sites[x_min:x_min+2*r, ...])
         else:
             x, y, z = np.where(sites[x_min:x_min+2*r, ...])
         if len(x) == 0:
             break
+        # Find all locations with the minimum x value
         options = np.where(x == x.min())[0]
+        # Choose of the locations
         choice = np.random.randint(len(options))
+        # Fetch the full coordinates of the center
         if im.ndim == 2:
             cen = np.vstack([x[options[choice]] + x_min,
                              y[options[choice]]])
@@ -120,13 +135,25 @@ def pseudo_gravity_packing(
             cen = np.vstack([x[options[choice]] + x_min,
                              y[options[choice]],
                              z[options[choice]]])
-        im_temp = _insert_disks_at_points(im_temp, coords=cen,
-                                          radii=np.array([r - clearance]),
-                                          v=True, overwrite=True)
-        sites = _insert_disks_at_points(sites, coords=cen,
-                                        radii=np.array([2*r]),
-                                        v=0,
-                                        overwrite=True)
+        # Insert spheres
+        im_temp = _insert_disks_at_points(
+            im_temp,
+            coords=cen,
+            radii=np.array([r]),
+            v=False,
+            overwrite=True,
+            smooth=smooth,
+        )
+        # Remove neighboring voxels (within 2R of sphere center) from list of sites
+        sites = _insert_disks_at_points(
+            sites,
+            coords=cen,
+            radii=np.array([2*r + 2*clearance]),
+            v=0,
+            overwrite=True,
+            smooth=smooth,
+        )
+        # Update x_min
         x_min += x.min()
     logger.debug(f'A total of {n} spheres were added')
     im_temp = np.swapaxes(im_temp, 0, axis)
@@ -246,17 +273,35 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     shape = [200, 200]
 
-    fig, ax = plt.subplots(1, 2)
-    im = ps.generators.pseudo_gravity_packing(im=np.ones(shape, dtype=bool),
-                                              r=7, clearance=3,
-                                              edges='contained', seed=0)
+    fig, ax = plt.subplots(1, 3)
+    im = ps.generators.pseudo_gravity_packing(
+        im=np.ones(shape, dtype=bool),
+        r=17,
+        clearance=11,
+        edges='contained',
+        seed=0,
+        maxiter=10,
+        smooth=False,
+    )
     ax[0].imshow(im, origin='lower')
-
-    sites = np.zeros(shape, dtype=bool)
-    sites[100, 100] = True
-    im = ps.generators.pseudo_electrostatic_packing(im=np.ones(shape,
-                                                               dtype=bool),
-                                                    r=5, sites=sites,
-                                                    clearance=4,
-                                                    maxiter=50, seed=0)
+    im = ps.generators.pseudo_gravity_packing(
+        im=im,
+        r=7,
+        clearance=0,
+        edges='extended',
+        seed=0,
+        maxiter=40,
+        smooth=False,
+    )
     ax[1].imshow(im, origin='lower')
+    im = ps.generators.pseudo_gravity_packing(
+        im=im,
+        r=10,
+        clearance=0,
+        edges='contained',
+        seed=0,
+        maxiter=50,
+        smooth=False,
+    )
+    ax[2].imshow(im, origin='lower')
+
