98a57a131279ddbec2047659ed595ed6a297b195
Amin Sadeghi <amin.sadeghi@live.com>
2024-03-11 12:09:05 -0400

    Unify more stuff in pyproject.toml

--- conftest.py
+++
@@ -1,8 +0,0 @@
-# Ignore the following during testing
-collect_ignore = [
-    "docs/conf.py",
-    "examples/generators/reference/fractal_noise.ipynb",
-    "examples/networks/reference/diffusive_size_factor_AI.ipynb",
-    "examples/networks/tutorials/predicting_diffusive_size_factors_rock_sample.ipynb",
-    "examples/networks/tutorials/using_diffusive_size_factor_AI_with_snow.ipynb",
-]
